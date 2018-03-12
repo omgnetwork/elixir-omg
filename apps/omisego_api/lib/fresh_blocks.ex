3ddef40dc30b22fdf80f1e96a5b10a1f66f569a3
pthomalla <pawel.thomalla@imapp.pl>
2018-03-12 13:09:50 +0100

    fixed dializer credo

--- apps/omisego_api/lib/fresh_blocks.ex
+++ apps/omisego_api/lib/fresh_blocks.ex
@@ -1,18 +1,19 @@
 defmodule OmiseGO.FreshBlocks do
   @moduledoc """
     Allows for quick access to a fresh subset of blocks
-    (do we need this?)
-    (mak it as wrapper to DB?)
   """
+  alias OmiseGO.DB
 
   # TODO remove this and import the true one
   defmodule Block do
+    @moduledoc """
+    """
     defstruct [:number]
   end
 
   ##### Client
-  def start_ling() do
-    GenServer.start_link(__MODULE__, :ok, __MODULE__)
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
   end
 
   @spec get(block_number :: integer) :: Block
@@ -20,34 +21,34 @@ defmodule OmiseGO.FreshBlocks do
     GenServer.call(__MODULE__, {:get, block_number})
   end
 
-  @spec push(block :: Block) :: boolean
-  def push(block) do
+  def push(%Block{} = block) do
     GenServer.cast(__MODULE__, {:push, block})
   end
 
   ##### Core
   defmodule Core do
+    @moduledoc """
+       core implementation
+    """
     defstruct container: %{}, max_size: 100, keys_queue: :queue.new()
 
     def get(block_number, state) do
       case Map.get(state.container, block_number) do
         nil -> {nil, [block_number]}
-        block -> {block, []}
+        %Block{} = block -> {block, []}
       end
     end
 
     def update(block, state) do
       keys_queue = :queue.in(block.number, state.keys_queue)
       container = Map.put(state.container, block.number, block)
-      cond do
-        state.max_size < Kernel.map_size(container) ->
-          {{:value, key_to_remove}, keys_queue} = :queue.out(state.keys_queue)
 
-          {:ok,
-           %{state | keys_queue: keys_queue, container: Map.delete(container, key_to_remove)}}
+      if state.max_size < Kernel.map_size(container) do
+        {{:value, key_to_remove}, keys_queue} = :queue.out(state.keys_queue)
 
-        true ->
-          {:ok, %{state | keys_queue: keys_queue, container: container}}
+        {:ok, %{state | keys_queue: keys_queue, container: Map.delete(container, key_to_remove)}}
+      else
+        {:ok, %{state | keys_queue: keys_queue, container: container}}
       end
     end
   end
@@ -56,17 +57,16 @@ defmodule OmiseGO.FreshBlocks do
   use GenServer
 
   def init(:ok) do
-    %Core{}
+    {:ok, %Core{}}
   end
 
-  # when is_integer(block_number)
-  def handle_call({:get, block_number}, _from, state) do
+  def handle_call({:get, block_number}, _from, %Core{} = state) do
     {block, blocks_to_fetch} = Core.get(block_number, state)
     fetched_blocks = DB.blocks(blocks_to_fetch)
     {:reply, Map.get(fetched_blocks, block_number, block), state}
   end
 
-  def handle_cast({:push, block = %Block{}}, _from, state) do
+  def handle_cast({:push, %Block{} = block}, _from, state) do
     {:ok, new_state} = Core.update(block, state)
     {:noreply, :ok, new_state}
   end
