236e250fb5ce68eec22ca98b6d1e8173e2bdb1db
Paul Peregud <paulperegud@gmail.com>
2018-03-13 15:43:56 +0100

    implementation of block queue
    * drop tracking of tx in mempool - just resubmit every time
    * proper API with docs
    * use simplified API for contract interaction
    * more tests

---
+++ apps/omisego_api/lib/eth.ex
@@ -0,0 +1,10 @@
+defmodule Eth do
+  @moduledoc false
+
+  def submit_block(_nonce, _hash, _gas), do: :ok
+
+  def get_ethereum_height, do: {:ok, 12345678}
+
+  def get_current_child_block, do: {:ok, 1005000}
+
+end
