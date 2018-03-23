f57a492b9d145adf02aacff4d987a3d723d151ea
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-03-23 13:05:20 +0100

    ugly leveldb integration test, utxo handling, start/stop

--- apps/omisego_db/lib/db/level_db_core.ex
+++ apps/omisego_db/lib/db/level_db_core.ex
@@ -11,9 +11,9 @@ defmodule OmiseGO.DB.LevelDBCore do
   end
 
   # TODO, switch to selecting the clause base on the type of tx/block/utxo (struct?)
-  defp parse_multi_update({:put, :tx, tx}), do: {:put, tx_key(tx), encode_value(tx)}
-  defp parse_multi_update({:put, :block, block}), do: {:put, block_key(block), encode_value(block)}
-  defp parse_multi_update({:put, :utxo, utxo}), do: {:put, utxo_key(utxo), encode_value(utxo)}
+  defp parse_multi_update({:put, :tx, tx}), do: {:put, tx_key(tx), encode_value(:tx, tx)}
+  defp parse_multi_update({:put, :block, block}), do: {:put, block_key(block), encode_value(:block, block)}
+  defp parse_multi_update({:put, :utxo, utxo}), do: {:put, utxo_key(utxo), encode_value(:utxo, utxo)}
 
   defp parse_multi_update({:delete, :tx, tx}), do: {:delete, tx_key(tx)}
   defp parse_multi_update({:delete, :block, block}), do: {:delete, block_key(block)}
@@ -21,9 +21,30 @@ defmodule OmiseGO.DB.LevelDBCore do
 
   def decode_value(:block, encoded), do: Poison.decode(encoded)
   def decode_value(:tx, encoded), do: Poison.decode(encoded)
-  def decode_value(:utxo, encoded), do: Poison.decode(encoded)
+  def decode_value(:utxo, encoded) do
+    with {:ok, %{"b" => b, "t" => t, "o" => o, "value" => utxo_value}} <- Poison.decode(encoded),
+         do: %{{b, t, o} => utxo_value}
+  end
 
-  defp encode_value(value), do: Poison.encode!(value)
+  defp encode_value(:tx, value), do: Poison.encode!(value)
+  defp encode_value(:block, value), do: Poison.encode!(value)
+  defp encode_value(:utxo, utxo) do
+    [{b, t, o}] = Map.keys(utxo)
+    [utxo_value] = Map.values(utxo)
+
+    %{b: b, t: t, o: o, value: utxo_value}
+    |> Poison.encode!
+  end
+
+  def filter_utxos(keys_stream) do
+    keys_stream
+    # |> Stream.map(&IO.inspect/1)
+    |> Stream.filter(fn
+      "u" <> _rest -> true
+      _ -> false
+    end)
+    # |> Stream.map(&IO.inspect/1)
+  end
 
   def tx_key(%{hash: hash} = _tx) do
     tx_key(hash)
@@ -43,6 +64,6 @@ defmodule OmiseGO.DB.LevelDBCore do
   end
   def utxo_key({blknum, txindex, oindex} = _utxo_id) do
     # FIXME: very bad, fix
-    <<"u", Integer.to_string(blknum), Integer.to_string(txindex), Integer.to_string(oindex)>>
+    "u" <> Integer.to_string(blknum) <> Integer.to_string(txindex) <> Integer.to_string(oindex)
   end
 end
