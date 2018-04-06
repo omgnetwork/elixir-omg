221c888c4d4db56f4d8ea17af812f0eb74980d8a
Pawel Urbanowicz Imapp <pawel.urbanowicz@imapp.pl>
2018-04-06 15:54:23 +0200

    fix: style

--- apps/omisego_watcher/lib/omisego_watcher/repo.ex
+++ apps/omisego_watcher/lib/omisego_watcher/repo.ex
@@ -5,8 +5,8 @@ defmodule OmiseGOWatcher.Repo do
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
   """
+
   def init(_, opts) do
     {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
   end
-  
 end
