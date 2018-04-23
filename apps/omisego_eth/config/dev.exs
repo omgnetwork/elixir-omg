f68ffcfcebc05ad48fc726b479a61959cb4bd3cf
Paul Peregud <paulperegud@gmail.com>
2018-04-23 11:32:43 +0200

    drop accidentally committed dev.exs file contents

--- apps/omisego_eth/config/dev.exs
+++ apps/omisego_eth/config/dev.exs
@@ -1,11 +1,5 @@
 use Mix.Config
+
 # File is automatically generated, don't edit!
 # To deploy contract and fill values below, run:
 # mix run --no-start -e 'OmiseGO.Eth.DevHelpers.prepare_dev_env()'
-
-config :omisego_eth,
-contract: "0x0dab318c38f6e791fc7bffb707f2acea454a97ee",
-txhash_contract: "0x3b1fed1c686adc94f25b294b447e581020759a1471e3357756722e8175b180b2",
-authority_addr: "0xe7239378ee7785be9ef1fb450b9149f366d66b84",
-root_path: "../../"
-
