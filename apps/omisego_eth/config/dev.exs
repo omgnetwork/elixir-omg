4dc26916d023fedd6a5fd47e2c8416ef7944df27
Pawel Gebal <pawel.gebal@imapp.pl>
2018-04-30 16:05:29 +0200

    Add Makefile to gitignore

--- apps/omisego_eth/config/dev.exs
+++ apps/omisego_eth/config/dev.exs
@@ -3,8 +3,3 @@ use Mix.Config
 # To deploy contract and fill values below, run:
 # mix run --no-start -e 'OmiseGO.Eth.DevHelpers.prepare_dev_env()'
 
-config :omisego_eth,
-contract: "0x8ff871d58df2f03c7fe65615c658d334cbb57cf4",
-txhash_contract: "0x5495046feb0d0e95c8f4bd399d4609fef5d2a7ac2d8f342ada515327e0902003",
-authority_addr: "0x8dc680d80f619b89cf7aebfc06a7abca0e94342e",
-root_path: "../../"
