16ee469d6cc24dc83a8ca233b205de1e88c3b619
pthomalla <pawel.thomalla@imapp.pl>
2018-05-07 14:42:47 +0200

    added comments in run_child

--- apps/omisego_watcher/run_child.sh
+++ apps/omisego_watcher/run_child.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#starts child chanin with defined config (use in TrackerOmisego.Fixtures)
+
 cd ../..
-echo -e "Script pid: $$\n\trun mix from: $PWD\n\twitch config: $@"
-#mix run --config $@ 2>&1
+echo -e "Script pid: $$\n\trun mix from: $PWD\n\twith config: $@"
+#do not run iex in the background, the parent process may want kill all child and this process
 iex --sname main -S mix run --config $@ 2>&1
