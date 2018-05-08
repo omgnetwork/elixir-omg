6cd122ad66f66a856e1387f03900a0bb9aa2516d
Pawel Gebal <pawel.gebal@imapp.pl>
2018-05-08 15:15:13 +0200

    Initialize db in Makefile env task

---
+++ apps/omisego_db/init_db.exs
@@ -0,0 +1,2 @@
+:ok = OmiseGO.DB.multi_update([{:put, :last_deposit_block_height, 0}])
+:ok = OmiseGO.DB.multi_update([{:put, :child_top_block_number, 0}])
--- watcher_tests.sh
+++ watcher_tests.sh
@@ -3,7 +3,8 @@ geth --dev --dev.period 2 --rpc --rpcapi personal,web3,eth &
 GETH_PID=$!
 sleep 3
 mix run --no-start -e 'OmiseGO.Eth.DevHelpers.prepare_test_env()'
-cd apps/omisego_watcher
+cd apps/omisego_db; mix run init_db.exs
+cd ../../apps/omisego_watcher
 mix test --only watcher_tests
 RESULT=$?
 kill -9 $GETH_PID
