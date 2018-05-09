db2abd719121b71a875b37e51cfa2c4513737bb7
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-05-09 14:32:17 +0200

    use request_timeout branch of ethereumex

--- apps/omisego_eth/config/config.exs
+++ apps/omisego_eth/config/config.exs
@@ -6,6 +6,7 @@ config :ethereumex,
   scheme: "http",
   host: "localhost",
   port: 8545,
-  url: "http://localhost:8545"
+  url: "http://localhost:8545",
+  request_timeout: 5000
 
 import_config "#{Mix.env}.exs"
