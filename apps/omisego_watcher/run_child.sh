9d127bf4fd46189bad171c2c53fa8808a232b25a
pthomalla <pawel.thomalla@imapp.pl>
2018-05-09 13:05:41 +0200

    deleted run_child.sh

--- apps/omisego_watcher/run_child.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#starts child chanin with defined config (use in TrackerOmisego.Fixtures)
-
-cd ../..
-echo -e "Script pid: $$\n\trun mix from: $PWD\n\twith config: $@"
-#do not run iex in the background, the parent process may want kill all child and this process
-iex --sname main -S mix run --config $@ 2>&1
