6b07f6a85c17093967d3d54b801bf7f735356ea2
Pawel Gebal <pawel.gebal@imapp.pl>
2018-05-10 13:50:37 +0200

    Separate database config for operator and watcher

--- apps/omisego_api/config/config.exs
+++ apps/omisego_api/config/config.exs
@@ -8,4 +8,7 @@ config :omisego_api,
 config :omisego_eth,
   child_block_interval: 1000
 
+config :omisego_db,
+  leveldb_path: Path.join([System.get_env("HOME"), ".omisego/operator_data"])
+
 #import_config "#{Mix.env}.exs"
