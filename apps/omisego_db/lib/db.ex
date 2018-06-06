defae927c82a1c6f557abf0b658353217a4b93c9
pthomalla <pawel.thomalla@imapp.pl>
2018-06-06 13:36:47 +0200

    error instead exception

--- apps/omisego_db/lib/db.ex
+++ apps/omisego_db/lib/db.ex
@@ -44,14 +44,14 @@ defmodule OmiseGO.DB do
     path = Application.get_env(:omisego_db, :leveldb_path)
 
     if !Enum.empty?(File.ls!(path)) do
-      raise("creatin omisego_db \n\tfolder should be empty:\t" <> path)
+      {:error, :folder_not_empty}
+    else
+      {:ok, started_apps} = Application.ensure_all_started(:omisego_db)
+      :ok = OmiseGO.DB.multi_update([{:put, :last_deposit_block_height, 0}])
+      :ok = OmiseGO.DB.multi_update([{:put, :last_fast_exit_block_height, 0}])
+      :ok = OmiseGO.DB.multi_update([{:put, :last_slow_exit_block_height, 0}])
+      :ok = OmiseGO.DB.multi_update([{:put, :child_top_block_number, 0}])
+      started_apps |> Enum.reverse() |> Enum.map(fn app -> :ok = Application.stop(app) end)
     end
-
-    {:ok, started_apps} = Application.ensure_all_started(:omisego_db)
-    :ok = OmiseGO.DB.multi_update([{:put, :last_deposit_block_height, 0}])
-    :ok = OmiseGO.DB.multi_update([{:put, :last_fast_exit_block_height, 0}])
-    :ok = OmiseGO.DB.multi_update([{:put, :last_slow_exit_block_height, 0}])
-    :ok = OmiseGO.DB.multi_update([{:put, :child_top_block_number, 0}])
-    started_apps |> Enum.reverse() |> Enum.map(fn app -> :ok = Application.stop(app) end)
   end
 end
