582ac53b5b95d8175e4a97fc37b024ad65de0595
Pawel Urbanowicz Imapp <pawel.urbanowicz@imapp.pl>
2018-07-09 17:36:33 +0200

    add byzantine events

--- apps/omisego_watcher/lib/omisego_watcher/eventer/event.ex
+++ apps/omisego_watcher/lib/omisego_watcher/eventer/event.ex
@@ -1,5 +1,8 @@
 defmodule OmiseGOWatcher.Eventer.Event do
-  @type t :: OmiseGOWatcher.Eventer.Event.AddressReceived.t()
+  @type t :: OmiseGOWatcher.Eventer.Event.AddressReceived.t() |
+             OmiseGOWatcher.Eventer.Event.InvalidBlock.t() |
+             OmiseGOWatcher.Eventer.Event.BlockWithHolding.t() |
+             OmiseGOWatcher.Eventer.Event.InvalidExit.t()
 
   defmodule AddressReceived do
     @moduledoc """
@@ -11,7 +14,44 @@ defmodule OmiseGOWatcher.Eventer.Event do
     defstruct [:tx, :child_blknum, :child_block_hash, :submited_at_ethheight]
 
     @type t :: %AddressReceived{
-            tx: any()
-          }
+                 tx: any()
+               }
   end
+
+  defmodule InvalidBlock do
+    @moduledoc """
+    Notifies about invalid block
+    """
+
+    def name, do: "invalid_block"
+
+    defstruct []
+
+    @type t :: %InvalidBlock{}
+  end
+
+  defmodule BlockWithHolding do
+    @moduledoc """
+    Notifies about block-withholding
+    """
+
+    def name, do: "block_withholding"
+
+    defstruct []
+
+    @type t :: %BlockWithHolding{}
+  end
+
+  defmodule InvalidExit do
+    @moduledoc """
+    Notifies about invalid exit
+    """
+
+    def name, do: "invalid_exit"
+
+    defstruct []
+
+    @type t :: %InvalidExit{}
+  end
+
 end
