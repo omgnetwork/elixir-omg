ee37e2608f4eec53abe60c22961f2f37ef2b9928
Pawel Nowosielski <Pawel.Nowosielski@imapp.pl>
2018-07-23 16:22:36 +0200

    Cleaning - reverting from test settings

--- apps/omisego_api/config/config.exs
+++ apps/omisego_api/config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :omisego_api, fee_specs_file_path: "./fee_specs.json"
+config :omisego_api, fee_specs_file_path: "./../../fee_specs.json"
 config :omisego_eth, child_block_interval: 1000
 
 config :omisego_api,
