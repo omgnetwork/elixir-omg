96eddba3e920e65a679896e952dce493c889b74b
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-07-26 17:25:46 +0200

    fix build

--- apps/omisego_jsonrpc/lib/jsonrpc/exposer.ex
+++ apps/omisego_jsonrpc/lib/jsonrpc/exposer.ex
@@ -32,9 +32,7 @@ defmodule OmiseGO.JSONRPC.Exposer do
   end
 
   defp apply_call(module, fname, args) do
-    {duration, result} = :timer.tc(fn -> apply(module, fname, args) end)
-
-    case result do
+    case apply(module, fname, args) do
       # NOTE: let's treat all errors in the called API as internal errors, this seems legit
       {:ok, any} ->
         {:ok, any}
