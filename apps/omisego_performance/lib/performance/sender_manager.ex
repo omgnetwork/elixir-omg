175f80e35f4daf7651721cf26a52a51aea635a23
Pawel Nowosielski <Pawel.Nowosielski@imapp.pl>
2018-07-27 12:52:04 +0200

    Enforcing logging rule String interpolation should use inspect function
    Applied above rule to all found logger invocations with and exception when
    log message was concatenated with outout from exernal program, e.g.: geth logs

--- apps/omisego_performance/lib/performance/sender_manager.ex
+++ apps/omisego_performance/lib/performance/sender_manager.ex
@@ -31,7 +31,7 @@ defmodule OmiseGO.Performance.SenderManager do
   @spec init({integer, integer, binary}) :: {:ok, map()}
   def init({ntx_to_send, nusers, destdir}) do
     Process.flag(:trap_exit, true)
-    _ = Logger.debug(fn -> "init called with #{nusers} users, each to send #{ntx_to_send}" end)
+    _ = Logger.debug(fn -> "init called with #{inspect(nusers)} users, each to send #{inspect(ntx_to_send)}" end)
 
     senders =
       1..nusers
@@ -153,7 +153,7 @@ defmodule OmiseGO.Performance.SenderManager do
 
     stats = analyze(state)
     :ok = File.write(destfile, Poison.encode!(stats))
-    _ = Logger.info(fn -> "Performance statistics written to file: #{destfile}" end)
+    _ = Logger.info(fn -> "Performance statistics written to file: #{inspect(destfile)}" end)
     :ok
   end
 end
