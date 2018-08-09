9d48a21cf1ac96f698d4c777dd754cb9ecded308
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-08-09 16:58:48 +0200

    using ExUnitFixtures.load_fixture_files, using fork of ex_unit_fixtures

--- apps/omisego_watcher/test/test_helper.exs
+++ apps/omisego_watcher/test/test_helper.exs
@@ -1,8 +1,5 @@
 ExUnit.configure(exclude: [integration: true])
-Code.require_file("../omisego_eth/test/fixtures.exs")
-Code.require_file("../omisego_db/test/fixtures.exs")
-Code.require_file("../omisego_api/test/fixtures.exs")
 ExUnitFixtures.start()
-# need to do this in umbrella apps
-ExUnitFixtures.load_fixture_files()
+# loading all fixture files from the whole umbrella project
+ExUnitFixtures.load_fixture_files("../**/test/**/fixtures.exs")
 ExUnit.start()
