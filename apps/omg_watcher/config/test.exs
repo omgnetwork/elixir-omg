b38c04e86f09517176a08009453a312596b53d7e
Pawel Urbanowicz Imapp <pawel.urbanowicz@imapp.pl>
2018-10-29 09:34:52 +0100

    feat: add postgres to JenkinsFile, add deferred_config as deps

--- apps/omg_watcher/config/test.exs
+++ apps/omg_watcher/config/test.exs
@@ -6,12 +6,10 @@ config :omg_watcher, OMG.Watcher.Web.Endpoint,
   http: [port: 4001],
   server: false
 
-# Configure your database
 config :omg_watcher, OMG.Watcher.DB.Repo,
-  adapter: Sqlite.Ecto2,
-  database: "/tmp/omisego/ecto_simple_" <> Integer.to_string(:rand.uniform(10_000_000)) <> ".sqlite3"
-
-config :omg_watcher, OMG.Watcher.DB.Repo, pool: Ecto.Adapters.SQL.Sandbox
+  adapter: Ecto.Adapters.Postgres,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  url: {:system, "DATABASE_URL", "postgres://omisego_dev:omisego_dev@localhost/omisego_test"}
 
 config :omg_watcher, block_getter_height_sync_interval_ms: 1_000
 
