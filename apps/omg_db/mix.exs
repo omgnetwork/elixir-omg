bdcc668937a6ff35f6f0937f08fa5c51f7457f0b
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-11-05 10:56:53 +0100

    docs: set version to 0.0.1 in mix.exs'

--- apps/omg_db/mix.exs
+++ apps/omg_db/mix.exs
@@ -4,7 +4,7 @@ defmodule OMG.DB.MixProject do
   def project do
     [
       app: :omg_db,
-      version: "0.1.0",
+      version: "0.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
