1e52ff183451aa4e6dec747f768bf4a441b3cfcb
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-11-29 10:29:34 +0100

    fix: update eth/mix.exs to point to omisego branch of contracts

--- apps/omg_eth/mix.exs
+++ apps/omg_eth/mix.exs
@@ -40,7 +40,7 @@ defmodule OMG.Eth.MixProject do
       {:briefly, "~> 0.3"},
       {
         :plasma_contracts,
-        git: "https://github.com/pdobacz/plasma-contracts",
+        git: "https://github.com/omisego/plasma-contracts",
         branch: "finalization_challenge_events",
         sparse: "contracts/",
         compile: contracts_compile(),
