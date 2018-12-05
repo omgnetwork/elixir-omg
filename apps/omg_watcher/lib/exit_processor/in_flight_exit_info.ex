7bce057a2d0064232580f990a498a45f54a3f858
Piotr Żelazko <piotr.zelazko@imapp.pl>
2018-12-05 13:36:23 +0100

    feat: add IFE structure

---
+++ apps/omg_watcher/lib/exit_processor/in_flight_exit_info.ex
@@ -0,0 +1,45 @@
+# Copyright 2018 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.Watcher.ExitProcessor.InFlightExitInfo do
+  @moduledoc """
+  Represents the bulk of information about a tracked in-flight exit.
+
+  Internal stuff of `OMG.Watcher.ExitProcessor`
+  """
+
+  alias OMG.API.State.Transaction
+  alias OMG.API.Utxo
+
+  defstruct [
+    :tx,
+    :tx_pos,
+    :timestamp,
+    # piggybacking
+    exit_map: 0..7 |> Enum.map(&{&1, %{is_piggybacked: false, is_finalized: false}}) |> Map.new(),
+    oldest_competitor: 0,
+    is_canonical: true,
+    is_finalized: false
+  ]
+
+  @type t :: %__MODULE__{
+          tx: Transaction.Signed.t(),
+          tx_pos: Utxo.Position.t(),
+          timestamp: non_neg_integer(),
+          exit_map: %{non_neg_integer() => %{is_piggybacked: boolean(), is_finalized: boolean()}},
+          oldest_competitor: non_neg_integer(),
+          is_canonical: boolean(),
+          is_finalized: boolean(),
+        }
+end
