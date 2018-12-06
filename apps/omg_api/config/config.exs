62d8250db147751398169a863423b63bac02ac16
Jake Bunce <jake@omise.co>
2018-12-06 17:57:44 +0700

    [elixir-omg] Add Sentry support for omg_api

--- apps/omg_api/config/config.exs
+++ apps/omg_api/config/config.exs
@@ -10,4 +10,16 @@ config :omg_api,
   child_block_minimal_enquque_gap: 4,
   fee_specs_file_path: "./fee_specs.json"
 
+config :sentry,  
+  dsn: {:system, "SENTRY_DSN"},
+  environment_name: Mix.env(),
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!(),
+  tags: %{
+    env: Mix.env(),
+    application: Mix.Project.config()[:app]
+  },
+  server_name: elem(:inet.gethostname(), 1),
+  included_environments: [:prod, :dev]
+
 import_config "#{Mix.env()}.exs"
