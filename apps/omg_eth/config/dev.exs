2ac7ae74bcb31d31257a89eb924191e0feb1a3a7
Jake Bunce <jake@omise.co>
2019-01-08 15:10:32 +0700

    [elixir-omg] Fix syntax error in apps/omg_eth/config/dev.exs

--- apps/omg_eth/config/dev.exs
+++ apps/omg_eth/config/dev.exs
@@ -2,5 +2,5 @@ use Mix.Config
 
 # bumping because otherwise import/unlock of accounts will fail in some of use cases (perf tests)
 config :ethereumex,
-  request_timeout: 60_000
+  request_timeout: 60_000,
   loggers: [Appsignal.Ecto, Ecto.LogEntry]
