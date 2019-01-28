9c96ce9d4ca95a2034f2bae3384bccc59af8db13
Jake Bunce <jake@omise.co>
2019-01-28 17:22:49 +0700

    fix: Syntax error in dev

--- apps/omg_watcher/config/dev.exs
+++ apps/omg_watcher/config/dev.exs
@@ -46,7 +46,7 @@ config :omg_watcher, OMG.Watcher.DB.Repo,
   timeout: 60_000,
   connect_timeout: 60_000,
   # DATABASE_URL format is following `postgres://{user_name}:{password}@{host:port}/{database_name}`
-  url: {:system, "DATABASE_URL", "postgres://omisego_dev:omisego_dev@localhost/omisego_dev"}
+  url: {:system, "DATABASE_URL", "postgres://omisego_dev:omisego_dev@localhost/omisego_dev"},
   loggers: [Appsignal.Ecto, Ecto.LogEntry]
 
 config :omg_watcher,
