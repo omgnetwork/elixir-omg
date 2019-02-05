7bc7d2dd066d70aed1bdb1c628accfc35063f84d
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2019-02-05 16:32:01 +0100

    refactor: separate calculation of OMG.DB key for a persistable item
    this is to prevent bugs as fixed in previous commit - now calculating a key for e.g.:
    `%Block`-like map (`key_for_item` function) 
    is not to be confused with calculating of a LevelDB-key from an app-key (block hash in this example, which is done by `key` function)

--- apps/omg_db/lib/leveldb_core.ex
+++ apps/omg_db/lib/leveldb_core.ex
@@ -38,12 +38,12 @@ defmodule OMG.DB.LevelDBCore do
 
   defp parse_multi_update({:put, :block, %{number: number, hash: hash} = item}) do
     [
-      {:put, key(:block, item), encode_value(:block, item)},
+      {:put, key_for_item(:block, item), encode_value(:block, item)},
       {:put, key(:block_hash, number), encode_value(:block_hash, hash)}
     ]
   end
 
-  defp parse_multi_update({:put, type, item}), do: [{:put, key(type, item), encode_value(type, item)}]
+  defp parse_multi_update({:put, type, item}), do: [{:put, key_for_item(type, item), encode_value(type, item)}]
   defp parse_multi_update({:delete, type, item}), do: [{:delete, key(type, item)}]
 
   defp decode_response(_type, db_response) do
@@ -87,17 +87,6 @@ defmodule OMG.DB.LevelDBCore do
   defp do_filter_keys(keys_stream, prefix),
     do: Stream.filter(keys_stream, fn {key, _} -> String.starts_with?(key, prefix) end)
 
-  def key(:block, %{hash: hash} = _block), do: key(:block, hash)
-  def key(:block, hash) when is_binary(hash), do: @keys_prefixes.block <> hash
-  def key(:utxo, {position, _utxo}), do: key(:utxo, position)
-  def key(:spend, {position, _blknum}), do: key(:spend, position)
-  def key(:exit_info, {position, _exit_info}), do: key(:exit_info, position)
-  def key(:in_flight_exit_info, {position, _info}), do: key(:in_flight_exit_info, position)
-  def key(:competitor_info, {position, _info}), do: key(:competitor_info, position)
-
-  def key(type, item) when type in @key_types,
-    do: Map.get(@keys_prefixes, type) <> :erlang.term_to_binary(item)
-
   @single_value_parameter_names [
     :child_top_block_number,
     :last_deposit_child_blknum,
@@ -117,5 +106,21 @@ defmodule OMG.DB.LevelDBCore do
     :last_ife_exit_finalizer_eth_height
   ]
 
+  @doc """
+  Produces a type-specific LevelDB key for a combination of type and type-agnostic/LevelDB-ignorant key
+  """
+  def key(:block, hash) when is_binary(hash), do: @keys_prefixes.block <> hash
   def key(parameter, _) when parameter in @single_value_parameter_names, do: Atom.to_string(parameter)
+
+  def key(type, specific_key) when type in @key_types,
+    do: Map.get(@keys_prefixes, type) <> :erlang.term_to_binary(specific_key)
+
+  # `key_for_item` gets the type-specific key to persist a whole item at, as used by `:put` updates
+  defp key_for_item(:block, %{hash: hash} = _block), do: key(:block, hash)
+  defp key_for_item(:utxo, {position, _utxo}), do: key(:utxo, position)
+  defp key_for_item(:spend, {position, _blknum}), do: key(:spend, position)
+  defp key_for_item(:exit_info, {position, _exit_info}), do: key(:exit_info, position)
+  defp key_for_item(:in_flight_exit_info, {position, _info}), do: key(:in_flight_exit_info, position)
+  defp key_for_item(:competitor_info, {position, _info}), do: key(:competitor_info, position)
+  defp key_for_item(parameter, value) when parameter in @single_value_parameter_names, do: key(parameter, value)
 end
