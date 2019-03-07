090c8f2f348ca44a83de3267af00ad51c9ed25bc
Ino Murko <ino.murko@outlook.com>
2019-03-07 09:41:48 +0100

    fix: omg watcher and api should have status as a umbrella dependency

--- apps/omg_api/mix.exs
+++ apps/omg_api/mix.exs
@@ -49,6 +49,7 @@ defmodule OMG.API.MixProject do
       {:merkle_tree, "~> 1.5.0"},
       {:deferred_config, "~> 0.1.1"},
       #
+      {:omg_status, in_umbrella: true},
       {:omg_db, in_umbrella: true},
       {:omg_eth, in_umbrella: true},
       {:omg_rpc, in_umbrella: true},
