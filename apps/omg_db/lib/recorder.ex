d888c728440aad6a1526fa73d3b5c1fea0a144f2
Ino Murko <ino.murko@outlook.com>
2019-03-25 14:12:42 +0100

    feature: getting persisted numbers on leveldb usage (read,multiread,write)

--- apps/omg_db/lib/recorder.ex
+++ apps/omg_db/lib/recorder.ex
@@ -18,6 +18,12 @@ defmodule OMG.DB.Recorder do
   """
   use GenServer
   @default_interval 5_000
+  @table __MODULE__
+  @write :write
+  @read :read
+  @multiread :multiread
+  @keys [{@write, to_charlist(@write)}, {@read, to_charlist(@read)}, {@multiread, to_charlist(@multiread)}]
+
   @type t :: %__MODULE__{
           name: atom(),
           parent: pid(),
@@ -25,7 +31,8 @@ defmodule OMG.DB.Recorder do
           interval: pos_integer(),
           reporter: (... -> atom()),
           tref: reference() | nil,
-          node: String.t() | nil
+          node: String.t() | nil,
+          table: atom()
         }
   defstruct name: nil,
             parent: nil,
@@ -33,13 +40,30 @@ defmodule OMG.DB.Recorder do
             interval: @default_interval,
             reporter: &Appsignal.set_gauge/3,
             tref: nil,
-            node: nil
+            node: nil,
+            table: @table
+
+  @spec update_write :: integer()
+  def update_write(table \\ @table) do
+    :ets.update_counter(table, @write, {2, 1}, {@write, 0})
+  end
+
+  @spec update_read :: integer()
+  def update_read(table \\ @table) do
+    :ets.update_counter(table, @read, {2, 1}, {@read, 0})
+  end
+
+  @spec update_multiread :: integer()
+  def update_multiread(table \\ @table) do
+    :ets.update_counter(table, @multiread, {2, 1}, {@multiread, 0})
+  end
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: opts.name)
   end
 
   def init(opts) do
+    table = create_stats_table(opts)
     {:ok, tref} = :timer.send_interval(opts.interval, self(), :gather)
 
     {:ok,
@@ -48,14 +72,49 @@ defmodule OMG.DB.Recorder do
        | key: to_charlist(opts.name),
          interval: get_interval(opts.name) || @default_interval,
          tref: tref,
-         node: Atom.to_string(:erlang.node())
+         node: Atom.to_string(:erlang.node()),
+         table: table
      }}
   end
 
   def handle_info(:gather, state) do
     # invoke the reporter function and pass the key and value (invoke the fn)
     _ = state.reporter.(state.key, Process.info(state.parent, :message_queue_len) |> elem(1), %{node: state.node})
+
+    _ =
+      Enum.each(@keys, fn {table_key, key} ->
+        case :ets.take(state.table, table_key) do
+          [{^table_key, value}] -> _ = state.reporter.(key, value, %{node: state.node})
+          _ -> :ok
+        end
+      end)
+
     {:noreply, state}
+  end
+
+  @spec create_stats_table(t) :: atom()
+  def create_stats_table(%{name: __MODULE__}) do
+    case :ets.whereis(@table) do
+      :undefined ->
+        true = @table == :ets.new(@table, table_settings())
+
+        @table
+
+      _ ->
+        @table
+    end
+  end
+
+  def create_stats_table(%{name: name}) do
+    case :ets.whereis(name) do
+      :undefined ->
+        true = name == :ets.new(name, table_settings())
+
+        name
+
+      _ ->
+        name
+    end
   end
 
   # check configuration and system env variable, otherwise use the default
@@ -72,4 +131,6 @@ defmodule OMG.DB.Recorder do
         num
     end
   end
+
+  defp table_settings, do: [:named_table, :set, :public, write_concurrency: true]
 end
