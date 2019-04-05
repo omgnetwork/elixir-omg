1a43348ac2b05869449823f9dbff69932e237167
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2019-04-05 11:54:47 +0200

    fix: bump the ridiculously small exit_processor_sla_margin
    in order to not have spurious unchallenged_exit reports

--- apps/omg_watcher/config/config.exs
+++ apps/omg_watcher/config/config.exs
@@ -9,8 +9,8 @@ use Mix.Config
 config :omg_watcher,
   namespace: OMG.Watcher,
   ecto_repos: [OMG.Watcher.DB.Repo],
-  # an hour worth of blocks - this is how long the child chain server has to block spends from exiting utxos
-  exit_processor_sla_margin: 4 * 60,
+  # an almost day worth of blocks - this is how long the child chain server has to block spends from exiting utxos
+  exit_processor_sla_margin: 23 * 4 * 60,
   maximum_block_withholding_time_ms: 1_200_000,
   block_getter_loops_interval_ms: 500,
   maximum_number_of_unapplied_blocks: 50,
