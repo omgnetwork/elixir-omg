f6f39ee9246ebc86e76026d1e7792a41b5ff5d6a
Thibault Denizet <thibault@omise.co>
2019-04-08 21:32:30 +1000

    Change creating_txhash to a compound index

--- apps/omg_watcher/priv/repo/migrations/20190408131000_add_missing_indices_to_txoutputs.exs
+++ apps/omg_watcher/priv/repo/migrations/20190408131000_add_missing_indices_to_txoutputs.exs
@@ -2,7 +2,7 @@ defmodule OMG.Watcher.Repo.Migrations.AddMissingIndicesToTxOuputs do
   use Ecto.Migration
 
   def change do
-    create index(:txoutputs, [:creating_txhash])
+    create index(:txoutputs, [:creating_txhash, :spending_txhash])
     create index(:txoutputs, [:creating_deposit])
     create index(:txoutputs, [:spending_txhash])
     create index(:txoutputs, [:spending_exit])
