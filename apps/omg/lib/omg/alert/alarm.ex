399be04f73ee5fd3f20b08e2040e837c161dcc5d
Arthur Chiu <hallo@achiurizo.co>
2019-05-06 13:28:04 -0700

    restrict Alarm#set/1 type /HT @pthomalla

--- apps/omg/lib/omg/alert/alarm.ex
+++ apps/omg/lib/omg/alert/alarm.ex
@@ -21,7 +21,7 @@ defmodule OMG.Alert.Alarm do
   @typedoc """
   The raw alarm being used to `set` the Alarm
   """
-  @type raw_t :: {:boot_in_progress | :ethereum_client_connection, any(), any()}
+  @type raw_t :: {:boot_in_progress | :ethereum_client_connection, node(), module()}
 
   def ethereum_client_connection_issue(node, reporter),
     do: {:ethereum_client_connection, %{node: node, reporter: reporter}}
@@ -30,7 +30,7 @@ defmodule OMG.Alert.Alarm do
     do: {:boot_in_progress, %{node: node, reporter: reporter}}
 
   @spec set(raw_t()) :: :ok | :duplicate
-  def set(raw_alarm) do
+  def set(raw_alarm = {_, node, reporter}) when is_atom(node) and is_atom(reporter) do
     alarm = make_alarm(raw_alarm)
     do_raise(alarm)
   end
