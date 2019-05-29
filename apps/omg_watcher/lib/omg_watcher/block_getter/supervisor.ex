2493178a034cc0d76bfa37bdf8b32935d301c317
Ino Murko <ino.murko@outlook.com>
2019-05-29 21:07:11 +0200

    fix: fixing linux watcher and lowering the amount of restarts for blockgetter

--- apps/omg_watcher/lib/omg_watcher/block_getter/supervisor.ex
+++ apps/omg_watcher/lib/omg_watcher/block_getter/supervisor.ex
@@ -37,7 +37,7 @@ defmodule OMG.Watcher.BlockGetter.Supervisor do
       }
     ]
 
-    opts = [strategy: :one_for_all, max_restarts: 1000, max_seconds: 60]
+    opts = [strategy: :one_for_all, max_restarts: 500, max_seconds: 5]
 
     _ = Logger.info("Starting #{inspect(__MODULE__)}")
     Supervisor.init(children, opts)
