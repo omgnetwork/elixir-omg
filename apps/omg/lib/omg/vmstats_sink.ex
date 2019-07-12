160645150b3614f25ac9792fc9bf0446ba6a60c6
Arthur Chiu <hallo@achiurizo.co>
2019-07-12 18:04:58 +0000

    fix sink methods

--- apps/omg/lib/omg/vmstats_sink.ex
+++ apps/omg/lib/omg/vmstats_sink.ex
@@ -3,14 +3,14 @@ defmodule OMG.VmstatsSink do
   @behaviour :vmstats_sink
 
   def collect(:counter, key, value) do
-    OMG.Utils.Metrics.Statix.set(key, value)
+    OMG.Utils.Metrics.set(key, value)
   end
 
   def collect(:gauge, key, value) do
-    OMG.Utils.Metrics.Statix.gauge(key, value)
+    OMG.Utils.Metrics.gauge(key, value)
   end
 
   def collect(:timing, key, value) do
-    OMG.Utils.Metrics.Statix.timing(key, value)
+    OMG.Utils.Metrics.timing(key, value)
   end
 end
