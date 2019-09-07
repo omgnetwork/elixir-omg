fbf1185a1e2977d91102f0a3b5bcf299685a72b9
Ino Murko <ino.murko@outlook.com>
2019-09-07 19:54:04 +0200

    fix: os mon settings for test

--- apps/omg_status/config/test.exs
+++ apps/omg_status/config/test.exs
@@ -9,3 +9,11 @@ config :omg_status, OMG.Status.Metric.Tracer,
   service: :omg_status,
   env: "test",
   disabled?: true
+
+# we don't want system alarms auto raised
+config :os_mon,
+  memsup_helper_timeout: 120,
+  memory_check_interval: 5,
+  system_memory_high_watermark: 0.99,
+  disk_almost_full_threshold: 0.99,
+  disk_space_check_interval: 120
