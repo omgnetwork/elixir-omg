7083cb136a74f17cd23da249b8dc80506e2ec6c2
Ino Murko <ino.murko@outlook.com>
2019-09-12 17:49:08 +0000

    fix: remove type from config

--- apps/omg_db/config/config.exs
+++ apps/omg_db/config/config.exs
@@ -3,7 +3,6 @@ use Mix.Config
 # see [here](README.md) for documentation
 
 config :omg_db,
-  type: :rocksdb,
   rocksdb: [server_module: OMG.DB.RocksDB.Server, server_name: OMG.DB.RocksDB.Server],
   metrics_collection_interval: 60_000
 
