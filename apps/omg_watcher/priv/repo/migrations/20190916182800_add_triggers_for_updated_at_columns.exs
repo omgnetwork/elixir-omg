49f05dcfecef2e3cd890297bdf0d3e3f1362335d
Jacob Hite <jrhite@gmail.com>
2019-09-16 18:42:19 +0700

    triggers for updating updated_at columns

---
+++ apps/omg_watcher/priv/repo/migrations/20190916182800_add_triggers_for_updated_at_columns.exs
@@ -0,0 +1,24 @@
+defmodule OMG.Watcher.DB.Repo.Migrations.AddTriggersForUpdatedAtColumns do
+  use Ecto.Migration
+
+  def change do
+    execute """
+      CREATE OR REPLACE FUNCTION update_updated_at_column()
+      RETURNS TRIGGER AS $$
+      BEGIN
+          NEW.updated_at = (now() at time zone 'utc');
+          RETURN NEW;
+      END;
+      $$ language 'plpgsql';
+
+      CREATE TRIGGER update_txoutputs_updated_at
+      BEFORE UPDATE ON txoutputs FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+      CREATE TRIGGER update_ethevents_updated_at
+      BEFORE UPDATE ON ethevents FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+      CREATE TRIGGER update_ethevents_txoutputs_updated_at
+      BEFORE UPDATE ON ethevents_txoutputs FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+    """
+  end
+end
