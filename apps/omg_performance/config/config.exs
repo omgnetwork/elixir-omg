0e7f0549fc619417d1771a906cdabc8623b65e9b
pthomalla <pthomalla@gmail.com>
2019-09-18 06:54:39 +0200

    test: add configuration for byzantine_events

--- apps/omg_performance/config/config.exs
+++ apps/omg_performance/config/config.exs
@@ -2,4 +2,8 @@ use Mix.Config
 
 config :briefly, directory: ["/tmp/omisego"]
 
+config :byzantine_events,
+  watcher_url: "localhost:7434",
+  child_chain_url: "localhost:9656"
+
 import_config "#{Mix.env()}.exs"
