6651357a32d5f9cfb2245aefd5c56720b56fa99c
Thibault Denizet <thibault@omise.co>
2019-09-20 11:41:41 +0700

    Get dialyzer to stop complaining about the type specs in Merkle

--- apps/omg/lib/omg/merkle.ex
+++ apps/omg/lib/omg/merkle.ex
@@ -24,7 +24,7 @@ defmodule OMG.Merkle do
 
   # Creates a Merkle proof that transaction under a given transaction index
   # is included in block consisting of hashed transactions
-  @spec create_tx_proof(list(binary()), non_neg_integer()) :: binary()
+  @spec create_tx_proof(list(String.t()), non_neg_integer()) :: binary()
   def create_tx_proof(hashed_txs, txindex) do
     build(hashed_txs)
     |> prove(txindex)
@@ -33,7 +33,7 @@ defmodule OMG.Merkle do
     |> Enum.join()
   end
 
-  @spec hash(list(binary())) :: binary()
+  @spec hash(list(String.t())) :: binary()
   def hash(hashed_txs) do
     MerkleTree.fast_root(hashed_txs,
       hash_function: &Crypto.hash/1,
