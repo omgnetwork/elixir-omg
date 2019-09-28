63159a192010b2e662de550eb47f42df6cd5ed0a
Ino Murko <ino.murko@outlook.com>
2019-09-28 13:00:24 +0200

    fix: extract function into a module, aliases

---
+++ apps/omg_child_chain/lib/omg_child_chain/coordinator_setup.ex
@@ -0,0 +1,31 @@
+# Copyright 2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.ChildChain.CoordinatorSetup do
+  @moduledoc """
+   The setup of `OMG.RootChainCoordinator` for the child chain server - configures the relations between different
+   event listeners
+  """
+
+  def coordinator_setup() do
+    deposit_finality_margin = Application.fetch_env!(:omg, :deposit_finality_margin)
+
+    %{
+      depositor: [finality_margin: deposit_finality_margin],
+      exiter: [waits_for: :depositor, finality_margin: 0],
+      in_flight_exit: [waits_for: :depositor, finality_margin: 0],
+      piggyback: [waits_for: :in_flight_exit, finality_margin: 0]
+    }
+  end
+end
