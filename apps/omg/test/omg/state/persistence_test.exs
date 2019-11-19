09db0d85f0adce1e862f96c7bfc283d7a3dbdd1a
Pawel Nowosielski <Pawel.Nowosielski@imapp.pl>
2019-11-19 14:53:49 +0100

    test: rewrite persistent tests to use 'State' process api

--- apps/omg/test/omg/state/persistence_test.exs
+++ apps/omg/test/omg/state/persistence_test.exs
@@ -17,14 +17,14 @@ defmodule OMG.State.PersistenceTest do
   Test focused on the persistence bits of `OMG.State.Core`
   """
   use ExUnitFixtures
-  use OMG.DB.RocksDBCase, async: true
+  use ExUnit.Case, async: false
+  use OMG.Utils.LoggerExt
 
   alias OMG.Block
   alias OMG.InputPointer
-  alias OMG.State.Core
   alias OMG.State.Transaction
-  alias OMG.State.UtxoSet
   alias OMG.Utxo
+  alias Support.WaitFor
 
   import OMG.TestHelper
 
@@ -34,174 +34,205 @@ defmodule OMG.State.PersistenceTest do
   @interval OMG.Eth.RootChain.get_child_block_interval() |> elem(1)
   @blknum1 @interval
 
-  setup %{db_pid: db_pid} do
-    :ok = OMG.DB.initiation_multiupdate(db_pid)
+  setup do
+    db_path = Briefly.create!(directory: true)
+    Application.put_env(:omg_db, :path, db_path, persistent: true)
+
+    :ok = OMG.DB.init()
+    {:ok, started_apps} = Application.ensure_all_started(:omg_db)
+    {:ok, bus_apps} = Application.ensure_all_started(:omg_bus)
+
+    {:ok, _} = Supervisor.start_link([{OMG.State, []}], strategy: :one_for_one)
+
+    on_exit(fn ->
+      Application.put_env(:omg_db, :path, nil)
+
+      (started_apps ++ bus_apps)
+      |> Enum.reverse()
+      |> Enum.map(fn app -> :ok = Application.stop(app) end)
+    end)
+
+    {:ok, %{}}
   end
 
-  @tag fixtures: [:state_empty, :alice]
-  test "persists_deposits",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 2}], db_pid)
+  @tag fixtures: [:alice, :bob]
+  test "persists deposits and utxo is available after restart", %{alice: alice, bob: bob} do
+    [
+      %{owner: bob, currency: @eth, amount: 10, blknum: 1},
+      %{owner: alice, currency: @eth, amount: 20, blknum: 2}
+    ]
+    |> persist_deposit()
+
+    assert OMG.State.utxo_exists?(Utxo.position(2, 0, 0))
+
+    :ok = restart_state()
+
+    assert OMG.State.utxo_exists?(Utxo.position(1, 0, 0))
+    assert OMG.State.utxo_exists?(Utxo.position(2, 0, 0))
   end
 
-  @tag fixtures: [:alice, :state_empty]
-  test "spending produces db updates, that will make the state persist",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
+  @tag fixtures: [:alice]
+  test "utxos are persisted", %{alice: alice} do
+    [%{owner: alice, currency: @eth, amount: 20, blknum: 1}]
+    |> persist_deposit()
     |> exec(create_recovered([{1, 0, 0, alice}], @eth, [{alice, 3}]))
-    |> persist_form(db_pid)
+    |> persist_form()
+
+    assert not OMG.State.utxo_exists?(Utxo.position(1, 0, 0))
+    assert OMG.State.utxo_exists?(Utxo.position(@blknum1, 0, 0))
   end
 
-  @tag fixtures: [:alice, :bob, :state_empty]
-  test "spending produces db updates, that will make the state persist, for all inputs",
-       %{alice: alice, db_pid: db_pid, bob: bob, state_empty: state} do
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
+  @tag fixtures: [:alice, :bob]
+  test "utxos are available after restart", %{alice: alice, bob: bob} do
+    [%{owner: alice, currency: @eth, amount: 20, blknum: 1}]
+    |> persist_deposit()
     |> exec(create_recovered([{1, 0, 0, alice}], @eth, [{bob, 7}, {alice, 3}]))
     |> exec(create_recovered([{@blknum1, 0, 0, bob}, {@blknum1, 0, 1, alice}], @eth, [{bob, 10}]))
-    |> persist_form(db_pid)
+    |> persist_form()
 
-    assert [] = persisted_standard_exitable_utxos(alice, db_pid)
-    assert [%{amount: 10, blknum: @blknum1}] = persisted_standard_exitable_utxos(bob, db_pid)
+    :ok = restart_state()
+
+    assert not OMG.State.utxo_exists?(Utxo.position(@blknum1, 0, 0))
+    assert not OMG.State.utxo_exists?(Utxo.position(@blknum1, 0, 1))
+    assert OMG.State.utxo_exists?(Utxo.position(@blknum1, 1, 0))
   end
 
-  @tag fixtures: [:alice, :bob, :state_empty]
-  test "utxos are persisted and can be fetched from db",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
-
-    db_key = OMG.InputPointer.Protocol.to_db_key(Utxo.position(1, 0, 0))
-    assert {:ok, {^db_key, %{output: %{amount: 20}}}} = OMG.DB.utxo(db_key, db_pid)
-  end
-
-  @tag fixtures: [:alice, :bob, :state_empty]
-  test "all utxos get initialized by query result from db",
-       %{alice: alice, db_pid: db_pid, bob: bob, state_empty: state} do
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
-    |> persist_deposit([%{owner: bob.addr, currency: @eth, amount: 20, blknum: 2}], db_pid)
-    |> exec(create_recovered([{1, 0, 0, alice}], @eth, [{bob, 7}, {alice, 3}]))
-    |> persist_form(db_pid)
-  end
-
-  @tag fixtures: [:alice, :state_empty]
-  test "persists exiting",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
-    utxo_pos_exit_1 = Utxo.position(@blknum1, 0, 0)
-    utxo_pos_exit_2 = Utxo.position(@blknum1, 0, 1)
-    utxo_positions = [utxo_pos_exit_1, utxo_pos_exit_2]
-
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
-    |> exec(create_recovered([{1, 0, 0, alice}], @eth, [{alice, 3}]))
-    |> persist_form(db_pid)
-    |> persist_exit_utxos(utxo_positions, db_pid)
-  end
-
-  @tag fixtures: [:alice, :state_empty]
-  test "persists piggyback related exits",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
-    tx = create_recovered([{1, 0, 0, alice}], @eth, [{alice, 7}, {alice, 3}])
-
-    utxo_pos_exits_in_flight = [%{call_data: %{in_flight_tx: Transaction.raw_txbytes(tx)}}]
-
-    utxo_pos_exits_piggyback = [
-      %{tx_hash: Transaction.raw_txhash(tx), output_index: 0, omg_data: %{piggyback_type: :output}}
-    ]
-
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
-    |> exec(tx)
-    |> persist_form(db_pid)
-    |> persist_exit_utxos(utxo_pos_exits_in_flight, db_pid)
-    |> persist_exit_utxos(utxo_pos_exits_piggyback, db_pid)
-  end
-
-  @tag fixtures: [:alice, :state_empty]
-  test "persists ife related exits",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
-    tx = create_signed([{1, 0, 0, alice}], @eth, [{alice, 7}, {alice, 3}])
-
-    utxo_pos_exits_in_flight = [%{call_data: %{in_flight_tx: Transaction.raw_txbytes(tx)}}]
-
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
-    |> persist_exit_utxos(utxo_pos_exits_in_flight, db_pid)
-  end
-
-  @tag fixtures: [:alice, :state_empty]
-  test "tx with zero outputs will not be written to DB, but other stuff will!",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
+  @tag fixtures: [:alice]
+  test "zero amount transaction's output is persisted as any other output!", %{alice: alice} do
+    [%{owner: alice, currency: @eth, amount: 20, blknum: 1}]
+    |> persist_deposit()
     |> exec(create_recovered([{1, 0, 0, alice}], @eth, [{alice, 0}]))
-    |> persist_form(db_pid)
+    |> persist_form()
+
+    assert OMG.State.utxo_exists?(Utxo.position(@blknum1, 0, 0))
   end
 
-  @tag fixtures: [:alice, :state_empty]
-  test "blocks and spends are persisted",
-       %{alice: alice, db_pid: db_pid, state_empty: state} do
+  @tag fixtures: [:alice, :bob]
+  test "cannot double spend from the transactions within the same block", %{alice: alice, bob: bob} do
+    :ok = persist_deposit([%{owner: alice, currency: @eth, amount: 10, blknum: 1}])
+
+    # after the restart newly up state won't have deposit's utxo in memory
+    :ok = restart_state()
+
+    assert :ok == exec(create_recovered([{1, 0, 0, alice}], @eth, [{bob, 7}, {alice, 3}]))
+    assert :utxo_not_found == exec(create_recovered([{1, 0, 0, alice}], @eth, [{alice, 10}]))
+  end
+
+  @tag fixtures: [:alice]
+  test "blocks and spends are persisted", %{alice: alice} do
     tx = create_recovered([{1, 0, 0, alice}], @eth, [{alice, 3}])
 
-    state
-    |> persist_deposit([%{owner: alice.addr, currency: @eth, amount: 20, blknum: 1}], db_pid)
+    [%{owner: alice, currency: @eth, amount: 20, blknum: 1}]
+    |> persist_deposit()
     |> exec(tx)
-    |> persist_form(db_pid)
+    |> persist_form()
 
-    assert {:ok, [hash]} = OMG.DB.block_hashes([@blknum1], db_pid)
+    assert {:ok, [hash]} = OMG.DB.block_hashes([@blknum1])
 
-    assert {:ok, [db_block]} = OMG.DB.blocks([hash], db_pid)
+    :ok = restart_state()
+
+    assert {:ok, [db_block]} = OMG.DB.blocks([hash])
     %Block{number: @blknum1, transactions: [block_tx], hash: ^hash} = Block.from_db_value(db_block)
 
     assert {:ok, tx} == Transaction.Recovered.recover_from(block_tx)
 
     assert {:ok, 1000} ==
-             tx |> Transaction.get_inputs() |> hd() |> InputPointer.Protocol.to_db_key() |> OMG.DB.spent_blknum(db_pid)
+             tx |> Transaction.get_inputs() |> hd() |> InputPointer.Protocol.to_db_key() |> OMG.DB.spent_blknum()
   end
 
-  # loads the state from DB including UTXO set`
-  defp state_from(db_pid) do
-    {:ok, height_query_result} = OMG.DB.get_single_value(:child_top_block_number, db_pid)
-    {:ok, utxos_query_result} = OMG.DB.utxos(db_pid)
+  @tag fixtures: [:alice]
+  test "exiting utxo is deleted from state", %{alice: alice} do
+    utxo_positions = [
+      Utxo.position(@blknum1, 0, 0),
+      Utxo.position(@blknum1, 0, 1)
+    ]
 
-    {:ok, state} = Core.extract_initial_state(height_query_result, @interval)
-    Core.with_utxos(state, UtxoSet.init(utxos_query_result))
+    [%{owner: alice, currency: @eth, amount: 20, blknum: 1}]
+    |> persist_deposit()
+    |> exec(create_recovered([{1, 0, 0, alice}], @eth, [{alice, 3}]))
+    |> persist_form()
+    |> persist_exit_utxos(utxo_positions)
+
+    :ok = restart_state()
+
+    assert not OMG.State.utxo_exists?(Utxo.position(@blknum1, 0, 0))
+    assert not OMG.State.utxo_exists?(Utxo.position(@blknum1, 0, 1))
   end
 
-  defp persist_deposit(state, deposits, db_pid) do
-    {:ok, {_, db_updates}, state} = Core.deposit(deposits, state)
-    persist_common(state, db_updates, db_pid)
+  @tag fixtures: [:alice]
+  test "cannot spend just exited utxo", %{alice: alice} do
+    :ok = persist_deposit([%{owner: alice, currency: @eth, amount: 20, blknum: 1}])
+
+    {:ok, _, _} = OMG.State.exit_utxos([Utxo.position(1, 0, 0)])
+
+    # exit db_updates won't get persisted yet, but alice tries to spent it immediately
+    assert :utxo_not_found == exec(create_recovered([{1, 0, 0, alice}], @eth, [{alice, 20}]))
+
+    # retry above with empty in-memory utxoset
+    :ok = restart_state()
+
+    {:ok, _, _} = OMG.State.exit_utxos([Utxo.position(1, 0, 0)])
+    assert :utxo_not_found == exec(create_recovered([{1, 0, 0, alice}], @eth, [{alice, 20}]))
   end
 
-  defp persist_form(state, db_pid) do
-    {:ok, {_, _, db_updates}, state} = Core.form_block(@interval, state)
-    persist_common(state, db_updates, db_pid)
+  defp persist_deposit(deposits) do
+    {:ok, db_updates} =
+      deposits
+      |> make_deposits()
+      |> OMG.State.deposit()
+
+    :ok = OMG.DB.multi_update(db_updates)
   end
 
-  defp persist_exit_utxos(state, exit_infos, db_pid) do
-    assert {:ok, {db_updates, _}, state} = exit_infos |> Core.get_exiting_utxos(state) |> Core.exit_utxos(state)
-    persist_common(state, db_updates, db_pid)
+  defp persist_form(:ok), do: persist_form()
+
+  defp persist_form() do
+    OMG.State.form_block()
+
+    # because `form_block` is non-blocking operation we need to wait it finishes
+    # easiest to do this is to schedule another operation on `OMG.State` which is blocking
+    _ = OMG.State.utxo_exists?(Utxo.position(0, 0, 0))
+
+    :ok
   end
 
-  defp exec(state, tx) do
-    assert {:ok, _, state} = Core.exec(state, tx, :no_fees_required)
-    state
+  defp exec(:ok, tx), do: exec(tx)
+
+  defp exec(tx) do
+    case OMG.State.exec(tx, :no_fees_required) do
+      {:ok, _} -> :ok
+      {:error, reason} -> reason
+    end
   end
 
-  defp persisted_standard_exitable_utxos(address, db_pid) do
-    {:ok, utxos_query_result} = OMG.DB.utxos(db_pid)
-    Core.standard_exitable_utxos(utxos_query_result, address.addr)
+  defp persist_exit_utxos(:ok, exit_infos), do: persist_exit_utxos(exit_infos)
+
+  defp persist_exit_utxos(exit_infos) do
+    {:ok, db_updates, _} = OMG.State.exit_utxos(exit_infos)
+
+    :ok = OMG.DB.multi_update(db_updates)
   end
 
-  defp persist_common(state, db_updates, db_pid) do
-    assert :ok = OMG.DB.multi_update(db_updates, db_pid)
-    assert drop_recently_spent(state) == state_from(db_pid)
-    state
+  defp make_deposits(list) do
+    list
+    |> Enum.map(fn %{owner: owner, currency: currency, amount: amount, blknum: blknum} ->
+      %{
+        root_chain_txhash: <<blknum::256>>,
+        log_index: 0,
+        owner: owner.addr,
+        currency: currency,
+        amount: amount,
+        blknum: blknum
+      }
+    end)
   end
 
-  # NOTE: `recently_spent` is in-memory property of the %Core{}, which isn't persisted so it can't be recovered from db
-  defp drop_recently_spent(state), do: %Core{state | recently_spent: MapSet.new()}
+  defp restart_state() do
+    GenServer.stop(OMG.State)
+    WaitFor.ok(fn -> if(GenServer.whereis(OMG.State), do: :ok) end)
+
+    assert OMG.State |> GenServer.whereis() |> Kernel.is_pid()
+    _ = Logger.info("OMG.State restarted")
+    :ok
+  end
 end
