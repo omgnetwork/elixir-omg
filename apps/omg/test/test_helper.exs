256208ae0c39e8a5466179b98634894ff33bdb64
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2019-11-21 18:51:00 +0100

    feat: add propcheck-based signhash and encoding property test (WIP)

--- apps/omg/test/test_helper.exs
+++ apps/omg/test/test_helper.exs
@@ -18,5 +18,6 @@ umbrella_root_dir = Application.fetch_env!(:omg, :umbrella_root_dir)
 ExUnitFixtures.load_fixture_files(Path.join(umbrella_root_dir, "apps/*/test/**/fixtures.exs"))
 ExUnit.start()
 
+{:ok, _} = Application.ensure_all_started(:propcheck)
 {:ok, _} = Application.ensure_all_started(:briefly)
 {:ok, _} = Application.ensure_all_started(:erlexec)
