d9103e164d308aa5ebff568647830995a08cc767
Ino Murko <ino.murko@outlook.com>
2019-11-27 13:50:46 +0100

    Inomurko/end2end gen swagger (#1165)
    * fix: generate api code
    
    * fix: dont format or check generated code
    
    * fix: formatter fixes for generated code

--- .formatter.exs
+++ .formatter.exs
@@ -5,11 +5,11 @@
     "mix.exs",
     "apps/*/mix.exs",
     "apps/*/{lib,test,config}/**/*.{ex,exs}",
-    "priv/apps/*/{lib,test,config}/**/*.{ex,exs}",
-    "priv/apps/*/mix.exs",
     "priv/config/config.exs",
     "priv/mix.exs",
     "priv/features/contexts/*.exs",
-  ],
+  ]
+  ++ Path.wildcard("apps/*/mix.exs") -- (Path.wildcard("apps/watchers_informational_api/mix.exs") ++ Path.wildcard("apps/watcher_security_critical_api/mix.exs") ++ Path.wildcard("apps/child_chain_api/mix.exs"))
+  ++ Path.wildcard("apps/*/{lib,test,config}/**/*.{ex,exs}") -- (Path.wildcard("apps/watchers_informational_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("apps/watcher_security_critical_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("apps/child_chain_api/{lib,test,config}/**/*.{ex,exs}")),
   line_length: 120
 ]
