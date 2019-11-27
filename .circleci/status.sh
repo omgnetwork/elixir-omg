8e1ba64301334e1870122cc23d96ffae65569655
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2019-11-27 23:39:24 +0700

    fix: .circleci/status.sh exit code

--- .circleci/status.sh
+++ .circleci/status.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 retries=0
+status=1
 
 # Retries roughly every 5 seconds up to 5 minutes
-while [ $retries -le 60 ];  do
+while [ $retries -lt 60 ];  do
   alarms=$(make get-alarms)
   status=$?
   echo ${alarms}
+
   if [ "$status" -eq "0" ]; then
-    break;
-  else
-    retries=$(( $retries + 1 ))
-    sleep 5
+    exit 0
   fi
+
+  retries=$(( ${retries} + 1 ))
+  sleep 5
 done
+
+exit ${status}
