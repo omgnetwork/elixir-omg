05a6d57eaec75373faee96331bffcaa0b7251163
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2019-11-27 23:27:22 +0700

    fix: limit max get-alarms retry to 5 minutes so failed builds don't prolong indefinitely

--- .circleci/status.sh
+++ .circleci/status.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-while true;  do
+retries=0
+
+# Retries roughly every 5 seconds up to 5 minutes
+while [ $retries -le 60 ];  do
   alarms=$(make get-alarms)
   status=$?
   echo ${alarms}
   if [ "$status" -eq "0" ]; then
     break;
   else
+    retries=$(( $retries + 1 ))
     sleep 5
   fi
 done
