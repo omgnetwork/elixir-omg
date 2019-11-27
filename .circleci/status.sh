0f1b96637b354b000b66f4f9abfed944b9ad7d01
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2019-11-27 23:57:47 +0700

    fix: reduce make get-alarms retries to 1 minute

--- .circleci/status.sh
+++ .circleci/status.sh
@@ -3,8 +3,8 @@
 retries=0
 status=1
 
-# Retries roughly every 5 seconds up to 5 minutes
-while [ $retries -lt 60 ];  do
+# Retries roughly every 5 seconds up to 1 minute
+while [ $retries -lt 12 ];  do
   alarms=$(make get-alarms)
   status=$?
   echo ${alarms}
