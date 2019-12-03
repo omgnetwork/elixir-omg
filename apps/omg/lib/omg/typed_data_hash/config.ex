7e0a610afc1590d092e6e50966ea66e3038cf844
Ino Murko <ino.murko@outlook.com>
2019-12-03 10:31:56 +0100

    fix: barebone fix and signatures setup

--- apps/omg/lib/omg/typed_data_hash/config.ex
+++ apps/omg/lib/omg/typed_data_hash/config.ex
@@ -28,7 +28,6 @@ defmodule OMG.TypedDataHash.Config do
   """
   @spec domain_data_from_config() :: Tools.eip712_domain_t()
   def domain_data_from_config() do
-    # configuration from contract takes precedence, but if it's missing, the fallback addr will be used
     verifying_contract_addr =
       Application.get_env(:omg_eth, :contract_addr) |> Access.get(:plasma_framework) |> Encoding.from_hex()
 
--- apps/omg/test/omg/dependency_conformance/signature_test.exs
+++ apps/omg/test/omg/dependency_conformance/signature_test.exs
@@ -52,9 +52,7 @@ defmodule OMG.DependencyConformance.SignatureTest do
 
   setup_all do
     signtest_addr = <<25, 146, 92, 198, 69, 114, 15, 187, 97, 247, 99, 4, 238, 21, 80, 30, 49, 151, 243, 169>>
-    # Eth.Encoding.to_hex(signtest_addr)})
-    :ok = Application.put_env(:omg_eth, :contract_addr, %{plasma_framework: nil})
-
+    :ok = Application.put_env(:omg_eth, :contract_addr, %{plasma_framework: Eth.Encoding.to_hex(signtest_addr)})
     [contract: signtest_addr]
   end
 
