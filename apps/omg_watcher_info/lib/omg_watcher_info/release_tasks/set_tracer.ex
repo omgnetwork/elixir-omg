d192a2cced5fff216b7693513c0e8a1d53f9e789
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2019-12-04 12:06:29 +0700

    fix: mix format

--- apps/omg_watcher_info/lib/omg_watcher_info/release_tasks/set_tracer.ex
+++ apps/omg_watcher_info/lib/omg_watcher_info/release_tasks/set_tracer.ex
@@ -28,8 +28,7 @@ defmodule OMG.WatcherInfo.ReleaseTasks.SetTracer do
   end
 
   defp get_dd_disabled do
-    dd_disabled? =
-      validate_bool(get_env("DD_DISABLED"), Application.get_env(@app, OMG.WatcherInfo.Tracer)[:disabled?])
+    dd_disabled? = validate_bool(get_env("DD_DISABLED"), Application.get_env(@app, OMG.WatcherInfo.Tracer)[:disabled?])
 
     _ = Logger.info("CONFIGURATION: App: #{@app} Key: DD_DISABLED Value: #{inspect(dd_disabled?)}.")
     dd_disabled?
