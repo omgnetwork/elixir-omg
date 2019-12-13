b457657e5f1ff98f8d07f8b2efee8ea0183ba920
Jacob Hite <jrhite@gmail.com>
2019-12-13 08:34:23 +0700

    move test code to watcherinfo, introduce ExMachina

--- apps/omg_watcher_info/priv/repo/migrations/20190925132500_add_and_fix_timestamps.exs
+++ apps/omg_watcher_info/priv/repo/migrations/20190925132500_add_and_fix_timestamps.exs
@@ -2,8 +2,6 @@ defmodule OMG.Watcher.Repo.Migrations.AddAndFixTimestamps do
   use Ecto.Migration
 
   def change do
-    # the timestamp columns for this table were oringally added with the wrong precision,
-    # so we update it here to microsecond precision
     alter table(:ethevents) do
       remove(:inserted_at)
       remove(:updated_at)
@@ -13,8 +11,6 @@ defmodule OMG.Watcher.Repo.Migrations.AddAndFixTimestamps do
       timestamps([type: :utc_datetime_usec, default: "1970-01-01T00:00:00Z"])
     end
 
-    # the timestamp columns for this table were originally added with the wrong precision,
-    # so we update it here to microsecond precision
     alter table(:txoutputs) do
       remove(:inserted_at)
       remove(:updated_at)
@@ -24,19 +20,19 @@ defmodule OMG.Watcher.Repo.Migrations.AddAndFixTimestamps do
       timestamps([type: :utc_datetime_usec, default: "1970-01-01T00:00:00Z"])
     end
 
-    # timestamps removed from join table ethevents_txoutputs because ecto has poor support for join tables
-    # with extra columns. i'm not sure it adds much anyway
     alter table(:ethevents_txoutputs) do
-      modify(:inserted_at, :utc_datetime_usec, default: "1970-01-01T00:00:00Z")
-      modify(:updated_at, :utc_datetime_usec, default: "1970-01-01T00:00:00Z")
+      remove :inserted_at
+      remove :updated_at
+    end
+
+    alter table(:ethevents_txoutputs) do
+      timestamps([type: :utc_datetime_usec, default: "1970-01-01T00:00:00Z"])
     end
 
     alter table(:transactions) do
       remove(:sent_at)
 
       timestamps([type: :utc_datetime_usec, default: "1970-01-01T00:00:00Z"])
-
-      modify(:sent_at, type: :utc_datetime_usec, default: "1970-01-01T00:00:00Z"]
     end
 
     alter table(:blocks) do
