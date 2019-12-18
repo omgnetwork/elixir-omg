5fe6f9a7fda1cd07b6154b79db7dc6b1d02c8687
Mederic <mederic@omise.co>
2019-12-18 13:59:32 +0700

    feat: add more tests

---
+++ apps/omg_watcher_info/test/omg_watcher_info/http_rpc/adapter_test.exs
@@ -0,0 +1,61 @@
+# Copyright 2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.WatcherInfo.HttpRPC.AdapterTest do
+  use ExUnit.Case, async: false
+
+  alias OMG.WatcherInfo.HttpRPC.Adapter
+
+  @eth OMG.Eth.RootChain.eth_pseudo_address()
+
+  describe "get_unparsed_response_body/1" do
+    test "returns an unparsed body when successful" do
+      body = "{\r\n  \"success\": true,\r\n  \"data\": {\r\n    \"test\": \"something\"\r\n  }\r\n}"
+      assert {:ok, response} = Adapter.get_unparsed_response_body(%HTTPoison.Response{status_code: 200, body: body})
+      assert response == %{"test" => "something"}
+    end
+
+    test "returns a `client_error` error with the data when failed" do
+      body = "{\r\n  \"success\": false,\r\n  \"data\": {\r\n    \"test\": \"something\"\r\n  }\r\n}"
+      assert {:error, response} = Adapter.get_unparsed_response_body(%HTTPoison.Response{status_code: 200, body: body})
+      assert response == {:client_error, %{"test" => "something"}}
+    end
+
+    test "returns a `malformed_response` error with the data when the body is not recognized" do
+      body = "{\r\n  \"malformed\": \"body\"\r\n}"
+      assert {:error, response} = Adapter.get_unparsed_response_body(%HTTPoison.Response{status_code: 200, body: body})
+      assert response == {:malformed_response, %{"malformed" => "body"}}
+    end
+  end
+
+  describe "get_response_body/1" do
+    test "returns a body with the key parsed when successful" do
+      body = "{\r\n  \"success\": true,\r\n  \"data\": {\r\n    \"test\": \"something\"\r\n  }\r\n}"
+      assert {:ok, response} = Adapter.get_response_body(%HTTPoison.Response{status_code: 200, body: body})
+      assert response == %{test: "something"}
+    end
+
+    test "returns a `client_error` error with the data when failed" do
+      body = "{\r\n  \"success\": false,\r\n  \"data\": {\r\n    \"test\": \"something\"\r\n  }\r\n}"
+      assert {:error, response} = Adapter.get_response_body(%HTTPoison.Response{status_code: 200, body: body})
+      assert response == {:client_error, %{"test" => "something"}}
+    end
+
+    test "returns a `malformed_response` error with the data when the body is not recognized" do
+      body = "{\r\n  \"malformed\": \"body\"\r\n}"
+      assert {:error, response} = Adapter.get_response_body(%HTTPoison.Response{status_code: 200, body: body})
+      assert response == {:malformed_response, %{"malformed" => "body"}}
+    end
+  end
+end
