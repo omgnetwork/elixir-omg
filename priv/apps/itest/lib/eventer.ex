94958e220a8082bb6a78868ac9022ed5ad2cc922
Ino Murko <ino.murko@outlook.com>
2019-12-30 23:32:30 +0100

    credo

--- priv/apps/itest/lib/eventer.ex
+++ priv/apps/itest/lib/eventer.ex
@@ -6,6 +6,7 @@ defmodule Itest.Eventer do
 
   @subscription_id 1
 
+  require Logger
   #
   # Client API
   #
@@ -19,36 +20,11 @@ defmodule Itest.Eventer do
 
     case Process.whereis(name) do
       nil ->
-        ws_url = Keyword.fetch!(opts, :ws_url)
-
-        case WebSockex.start_link(ws_url, __MODULE__, opts, name: name) do
-          {:error, {:already_started, pid}} ->
-            {:ok, pid}
-
-          {:ok, pid} ->
-            spawn(fn -> listen(pid, opts) end)
-            {:ok, pid}
-        end
+        websockex_start_link(name, opts)
 
       pid ->
         {:ok, pid}
     end
-  end
-
-  # >> {"id": 1, "method": "eth_subscribe", "params": ["logs", {"address": "0x8320fe7702b96808f7bbc0d4a888ed1468216cfd", "topics": ["0xd78a0cb8bb633d06981248b816e7bd33c2a35a6089241d099fa519e361cab902"]}]}
-  defp listen(pid, opts) do
-    payload = %{
-      jsonrpc: "2.0",
-      id: @subscription_id,
-      method: "eth_subscribe",
-      params: [
-        "logs",
-        # %{"address" => Itest.Account.plasma_framework()}
-        Keyword.fetch!(opts, :listen_to)
-      ]
-    }
-
-    WebSockex.send_frame(pid, {:text, Jason.encode!(payload)})
   end
 
   #
@@ -67,11 +43,41 @@ defmodule Itest.Eventer do
   def handle_frame({:text, msg}, state) do
     {:ok, decoded} = Jason.decode(msg)
 
-    IO.inspect("got message on #{inspect(state)} msg -> ")
-    IO.inspect(decoded)
+    _ = Logger.info("got message on #{inspect(state)} msg -> #{inspect(decoded)} ")
     # listen_to = Keyword.fetch!(state, :listen_to)
     # event_bus = Keyword.fetch!(state, :event_bus)
     # :ok = apply(event_bus, :broadcast, [listen_to, {String.to_atom(listen_to), decoded}])
     {:ok, state}
+  end
+
+  defp websockex_start_link(name, opts) do
+    ws_url = Keyword.fetch!(opts, :ws_url)
+
+    case WebSockex.start_link(ws_url, __MODULE__, opts, name: name) do
+      {:error, {:already_started, pid}} ->
+        {:ok, pid}
+
+      {:ok, pid} ->
+        spawn(fn -> listen(pid, opts) end)
+        {:ok, pid}
+    end
+  end
+
+  # >> {"id": 1, "method": "eth_subscribe", "params": ["logs",
+  #  {"address": "0x8320fe7702b96808f7bbc0d4a888ed1468216cfd",
+  # "topics": ["0xd78a0cb8bb633d06981248b816e7bd33c2a35a6089241d099fa519e361cab902"]}]}
+  defp listen(pid, opts) do
+    payload = %{
+      jsonrpc: "2.0",
+      id: @subscription_id,
+      method: "eth_subscribe",
+      params: [
+        "logs",
+        # %{"address" => Itest.Account.plasma_framework()}
+        Keyword.fetch!(opts, :listen_to)
+      ]
+    }
+
+    WebSockex.send_frame(pid, {:text, Jason.encode!(payload)})
   end
 end
