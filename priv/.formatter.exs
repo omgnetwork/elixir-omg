3faaa40cb67c343a669fd6a59a211ffc7f21c1ef
Ino Murko <ino.murko@outlook.com>
2020-01-10 17:37:06 +0100

    fix: pr comments

--- priv/.formatter.exs
+++ priv/.formatter.exs
@@ -3,10 +3,7 @@
     "rel/config.exs",
     "mix.exs",
     "features/*/*.{ex,exs}",
-    "features/config.exs",
-    "apps/*/mix.exs",
-    "apps/*/{lib,test,config}/**/*.{ex,exs}",
-    "priv/config/config.exs"
+    "apps/itest/test/**/*.{ex,exs}",
   ]
   ++ (Path.wildcard("apps/*/mix.exs") -- ["apps/watcher_info_api/mix.exs", "apps/watcher_security_critical_api/mix.exs", "apps/child_chain_api/mix.exs"])
   ++ (Path.wildcard("apps/*/{lib,test,config}/**/*.{ex,exs}") -- (Path.wildcard("apps/watcher_info_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("apps/watcher_security_critical_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("apps/child_chain_api/{lib,test,config}/**/*.{ex,exs}"))),
