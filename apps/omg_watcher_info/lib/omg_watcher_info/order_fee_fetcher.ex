0cf5fa10fa3519c7ee7708eafd84b3445c389197
Mederic <mederic@omise.co>
2020-01-15 15:53:44 +0700

    feat add spec and doc

--- apps/omg_watcher_info/lib/omg_watcher_info/order_fee_fetcher.ex
+++ apps/omg_watcher_info/lib/omg_watcher_info/order_fee_fetcher.ex
@@ -17,14 +17,29 @@ defmodule OMG.WatcherInfo.OrderFeeFetcher do
   Handle fetching and formatting of fees for an order
   """
 
-  alias OMG.WatcherInfo.HttpRPC.Client
-  alias OMG.WireFormatTypes
+  alias OMG.Crypto
+  alias OMG.State.Transaction
   alias OMG.Utils.HttpRPC.Encoding
+  alias OMG.WatcherInfo.HttpRPC.Client
+  alias OMG.WatcherInfo.UtxoSelection
+  alias OMG.WireFormatTypes
 
   # Note: Hardcoding the tx_type for now
   @tx_type WireFormatTypes.tx_type_for(:tx_payment_v1)
   @str_tx_type Integer.to_string(@tx_type)
 
+  @type order_without_fee_amount_t() :: %{
+          owner: Crypto.address_t(),
+          payments: nonempty_list(UtxoSelection.payment_t()),
+          fee: %{currency: Transaction.Payment.currency()},
+          metadata: binary() | nil
+        }
+
+  @doc """
+  Fetch the correct fee amount for the given fee currency from the childchain
+  and adds it to the order map.
+  """
+  @spec add_fee_to_order(order_without_fee_amount_t()) :: {:ok, UtxoSelection.order_t()} | {:error, atom()}
   def add_fee_to_order(%{fee: %{currency: currency}} = order, url \\ nil) do
     child_chain_url = url || Application.get_env(:omg_watcher_info, :child_chain_url)
     encoded_currency = Encoding.to_hex(currency)
