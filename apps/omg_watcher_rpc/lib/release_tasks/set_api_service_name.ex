5571f509fc82e9eefc2769cf575e8c450492d52a
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2020-01-16 11:40:28 +0700

    wip

---
+++ apps/omg_watcher_rpc/lib/release_tasks/set_api_service_name.ex
@@ -0,0 +1,28 @@
+# Copyright 2019-2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.WatcherRPC.ReleaseTasks.SetApiServiceName do
+  @moduledoc false
+  use Distillery.Releases.Config.Provider
+  require Logger
+
+  @impl Provider
+
+  def init(args) do
+    case Map.get(args, :service_name) do
+      nil -> exit("WatcherRPC's service name not provided.")
+      service_name -> Application.put_env(:omg_watcher_rpc, :service_name, service_name)
+    end
+  end
+end
