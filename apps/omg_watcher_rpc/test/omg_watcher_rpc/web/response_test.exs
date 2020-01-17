0a6e8e5e7c8e01708e072095b9cfc227ad1537b8
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2020-01-17 11:14:50 +0700

    test: appending service_name and version to watcher api responses

---
+++ apps/omg_watcher_rpc/test/omg_watcher_rpc/web/response_test.exs
@@ -0,0 +1,44 @@
+# Copyright 2019-2020 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.WatcherRPC.Web.ResponseTest do
+  # async: false because it needs to manipulate the global :api_mode application env.
+  use ExUnit.Case, async: false
+  alias OMG.WatcherRPC.Web.Response
+
+  @app :omg_watcher_rpc
+
+  setup do
+    original_mode = Application.get_env(@app, :api_mode)
+
+    on_exit(fn ->
+      _ = Application.put_env(@app, :api_mode, original_mode)
+    end)
+  end
+
+  describe "add_app_infos/1" do
+    test "appends the given map with a service_name and version" do
+      :ok = Application.put_env(@app, :api_mode, :watcher)
+      assert %{foo: "bar", service_name: _, version: _} = Response.add_app_infos(%{foo: "bar"})
+    end
+
+    test "appends the given map with the correct service_name" do
+      :ok = Application.put_env(@app, :api_mode, :watcher)
+      assert %{foo: "bar", service_name: "watcher"} = Response.add_app_infos(%{foo: "bar"})
+
+      :ok = Application.put_env(@app, :api_mode, :watcher_info)
+      assert %{foo: "bar", service_name: "watcher_info"} = Response.add_app_infos(%{foo: "bar"})
+    end
+  end
+end
