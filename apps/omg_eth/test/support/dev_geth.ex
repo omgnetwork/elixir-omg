3912c914344766bee52a53876080cec1f2403fce
Ino Murko <ino.murko@outlook.com>
2020-02-17 18:00:07 +0100

    wrong place wrong time

--- apps/omg_eth/test/support/dev_geth.ex
+++ apps/omg_eth/test/support/dev_geth.ex
@@ -46,7 +46,8 @@ defmodule OMG.Eth.DevGeth do
             --unlock \"0,1\" \
             --rpc --rpcapi personal,web3,eth,net --rpcaddr 0.0.0.0 --rpcvhosts='*' --rpcport=8545 \
             --ws --wsaddr 0.0.0.0 --wsorigins='*' \
-            --mine --datadir --allow-insecure-unlock #{datadir} 2>&1)
+            --allow-insecure-unlock \
+            --mine --datadir #{datadir} 2>&1)
     geth_pid = launch(geth)
 
     {:ok, :ready} = WaitFor.eth_rpc()
