99fd93e97386bbf207ead1ffe488110ef3026a07
boolafish <boolafish945@gmail.com>
2020-02-19 20:53:23 +0900

    fix: formatter using * without wildcard (#1328)
    * fix: formatter using * without wildcard
    
    * refactor: better readibility on wildcard multiple paths
    
    kudo @pnowosie

--- .formatter.exs
+++ .formatter.exs
@@ -8,7 +8,10 @@
     "priv/*/config/config.exs",
     "priv/*/mix.exs",
   ]
-  ++ (Path.wildcard("priv/*/apps/*/mix.exs") -- ["priv/*/apps/watcher_info_api/mix.exs", "priv/*/apps/watcher_security_critical_api/mix.exs", "priv/*/apps/child_chain_api/mix.exs"])
+  ++ (Path.wildcard("priv/*/apps/*/mix.exs") -- Enum.flat_map(
+    ["priv/*/apps/watcher_info_api/mix.exs", "priv/*/apps/watcher_security_critical_api/mix.exs", "priv/*/apps/child_chain_api/mix.exs"],
+    &Path.wildcard/1
+  ))
   ++ (Path.wildcard("priv/*/apps/*/{lib,test,config}/**/*.{ex,exs}") -- (Path.wildcard("priv/*/apps/watcher_info_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("priv/*/apps/watcher_security_critical_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("priv/*/apps/child_chain_api/{lib,test,config}/**/*.{ex,exs}"))),
   line_length: 120
 ]
