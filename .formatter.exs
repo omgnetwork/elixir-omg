4896852df1a7ed1cfa4510191b93cba4da206af3
boolafish <boolafish945@gmail.com>
2020-02-19 15:55:16 +0900

    fix: formatter using * without wildcard

--- .formatter.exs
+++ .formatter.exs
@@ -8,7 +8,7 @@
     "priv/*/config/config.exs",
     "priv/*/mix.exs",
   ]
-  ++ (Path.wildcard("priv/*/apps/*/mix.exs") -- ["priv/*/apps/watcher_info_api/mix.exs", "priv/*/apps/watcher_security_critical_api/mix.exs", "priv/*/apps/child_chain_api/mix.exs"])
+  ++ (Path.wildcard("priv/*/apps/*/mix.exs") -- (Path.wildcard("priv/*/apps/watcher_info_api/mix.exs") ++ Path.wildcard("priv/*/apps/watcher_security_critical_api/mix.exs") ++ Path.wildcard("priv/*/apps/child_chain_api/mix.exs")))
   ++ (Path.wildcard("priv/*/apps/*/{lib,test,config}/**/*.{ex,exs}") -- (Path.wildcard("priv/*/apps/watcher_info_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("priv/*/apps/watcher_security_critical_api/{lib,test,config}/**/*.{ex,exs}") ++ Path.wildcard("priv/*/apps/child_chain_api/{lib,test,config}/**/*.{ex,exs}"))),
   line_length: 120
 ]
