47d63a7d3a55c5cb457f05cf1e6674e8c1b71434
Ino Murko <ino.murko@outlook.com>
2020-02-20 18:43:57 +0100

    double the max_header_value_length (#1331)
    * double the max_header_value_length
    
    * move comments around

--- apps/omg_watcher_rpc/config/config.exs
+++ apps/omg_watcher_rpc/config/config.exs
@@ -1,8 +1,3 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
 # In mix environment, all modules are loaded, therefore it behaves like a watcher_info
@@ -10,12 +5,19 @@ config :omg_watcher_rpc,
   api_mode: :watcher_info
 
 # Configures the endpoint
+# https://ninenines.eu/docs/en/cowboy/2.4/manual/cowboy_http/
+# defaults are:
+# protocol_options:[max_header_name_length: 64,
+# max_header_value_length: 4096,
+# max_headers: 100,
+# max_request_line_length: 8096
+# ]
 config :omg_watcher_rpc, OMG.WatcherRPC.Web.Endpoint,
   render_errors: [view: OMG.WatcherRPC.Web.Views.Error, accepts: ~w(json)],
   pubsub: [name: OMG.WatcherRPC.PubSub, adapter: Phoenix.PubSub.PG2],
   instrumenters: [SpandexPhoenix.Instrumenter],
   enable_cors: true,
-  http: [:inet6, port: 7434],
+  http: [:inet6, port: 7434, protocol_options: [max_request_line_length: 8192, max_header_value_length: 8192]],
   url: [host: "w.example.com", port: 80],
   code_reloader: false
 
