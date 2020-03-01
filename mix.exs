fa33d3279c2c14cb8209b0a1f01c8f004bc5ced1
Ino Murko <ino.murko@outlook.com>
2020-03-01 22:27:49 +0100

    credo

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule OMG.Umbrella.MixProject do
     [
       {:distillery, "~> 2.1", runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.0.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.2.3", only: [:dev, :test], runtime: false},
       # https://github.com/xadhoom/excoveralls.git `52c6c8e5d7fe9abb814e5e3e546c863b9b2b41b7` rebased on `master`
       # more or less around v0.11.1
       {:excoveralls,
