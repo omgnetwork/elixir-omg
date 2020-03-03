f87a6ee66165440587a9af0fbabb57c14442f91d
boolafish <boolafish945@gmail.com>
2020-03-03 19:08:56 +0900

    chore: add CI to priv/perf (#1358)
    * chore: add credo to priv/perf
    
    Can run `mix credo` now. Basically copying credo config from priv/cabbage.
    
    * chore: add line_length 120 to formatter for priv/perf
    
    * chore: add local perf test to ci
    
    * feat: multi config for different env
    
    * feat: add run_load_test_on_deployed_env target in circle ci
    
    1. adds load test to the deployed dev env.
    2. remove make test command and use mix test directly

---
+++ priv/perf/config/stress.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+config :load_test,
+  child_chain_url: "https://stress-e043a92-childchain-ropsten-01.omg.network/",
+  watcher_security_url: "https://stress-e043a92-watcher-ropsten-01.omg.network/",
+  watcher_info_url: "https://stress-e043a92-watcher-info-ropsten-01.omg.network/"
