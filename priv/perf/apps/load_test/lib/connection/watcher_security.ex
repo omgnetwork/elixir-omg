bf30e606113b6c950966f4650d20ea11d12fa6bd
boolafish <boolafish945@gmail.com>
2020-03-06 11:53:57 +0900

    chore: add retry to tesla to enhance stability of load test
    To avoid transient error breaking the laod test.

--- priv/perf/apps/load_test/lib/connection/watcher_security.ex
+++ priv/perf/apps/load_test/lib/connection/watcher_security.ex
@@ -3,13 +3,16 @@ defmodule LoadTest.Connection.WatcherSecurity do
   Module that overrides the Tesla middleware with the url in config.
   """
 
+  alias LoadTest.Connection.Utils
+
   def client() do
     base_url = Application.get_env(:load_test, :watcher_security_url)
 
     middleware = [
       {Tesla.Middleware.BaseUrl, base_url},
       {Tesla.Middleware.EncodeJson, engine: Jason},
-      {Tesla.Middleware.Headers, [{"user-agent", "Elixir"}]}
+      {Tesla.Middleware.Headers, [{"user-agent", "Elixir"}]},
+      {Tesla.Middleware.Retry, delay: 500, max_retries: 10, max_delay: 45_000, should_retry: Utils.retry?()}
     ]
 
     Tesla.client(middleware)
