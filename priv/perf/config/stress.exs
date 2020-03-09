cbd5b61a0f56db392a677c08752e6c4fcd66bc83
Piotr Żelazko <piotr.zelazko@imapp.pl>
2020-03-09 13:27:16 +0100

    chore: update configs

--- priv/perf/config/stress.exs
+++ priv/perf/config/stress.exs
@@ -1,8 +1,32 @@
 use Mix.Config
 
-config :load_test,
+config :ethereumex,
+  url: System.get_env("ETHEREUM_RPC_URL")
+
+config :omg_load_test,
   pool_size: 20_000,
   max_connection: 20_000,
   child_chain_url: "https://stress-e043a92-childchain-ropsten-01.omg.network/",
   watcher_security_url: "https://stress-e043a92-watcher-ropsten-01.omg.network/",
-  watcher_info_url: "https://stress-e043a92-watcher-info-ropsten-01.omg.network/"
+  watcher_info_url: "https://stress-e043a92-watcher-info-ropsten-01.omg.network/",
+  fee_wei: 1,
+  faucet_deposit_wei: (:math.pow(10, 18) * 1) |> trunc,
+  faucet_account: %{
+    addr: "0x9133f35d9a964c894f152c0e7da66e832735a7a6",
+    priv: "0x70ad9d48f90430607a340ea2e00495e1f84d2c50a4a3df0917e6161045b32378"
+  },
+  # 0.00000001 ETH
+  initial_funds_wei: :math.pow(10, 9) |> trunc()
+
+config :ex_plasma,
+  authority_address: "0x87b8602c67c4419905dd1054f822132e7aa1a3b4",
+  contract_address: "0x180ca7a9437e12fa4093a1a3d13648f25faad86f",
+  eth_vault_address: "0x7b3425f47f1970f5c670fee895062eaf5799e481",
+  ecr20_vault_address: "0x781c00fde3aa61fa44b991082c54619159634453",
+  exit_game_address: "0x90579da613437d3e1f994aab98ff46fc5b31ab6d",
+  eip_712_domain: [
+    name: "OMG Network",
+    salt: "0xfad5c7f626d80f9256ef01929f3beb96e058b8b4b0e3fe52d84f054c0e2a7a83",
+    verifying_contract: "0x180ca7a9437e12fa4093a1a3d13648f25faad86f",
+    version: "1"
+  ]
--- priv/perf/config/test.exs
+++ priv/perf/config/test.exs
@@ -1,6 +1,25 @@
 use Mix.Config
 
-config :load_test,
+config :logger, level: :info
+
+config :ethereumex,
+  url: "http://localhost:8545"
+
+config :omg_load_test,
   child_chain_url: "http://localhost:9656",
   watcher_security_url: "http://localhost:7434",
-  watcher_info_url: "http://localhost:7534"
+  watcher_info_url: "http://localhost:7534",
+  faucet_deposit_wei: (:math.pow(10, 18) * 10) |> trunc,
+  fee_wei: 1
+
+config :ex_plasma,
+  authority_address: "0xc0f780dfc35075979b0def588d999225b7ecc56f",
+  contract_address: "0xc673e4ffcb8464faff908a6804fe0e635af0ea2f",
+  eth_vault_address: "0x4e3aeff70f022a6d4cc5947423887e7152826cf7",
+  exit_game_address: "0x89afce326e7da55647d22e24336c6a2816c99f6b",
+  eip_712_domain: [
+    name: "OMG Network",
+    salt: "0xfad5c7f626d80f9256ef01929f3beb96e058b8b4b0e3fe52d84f054c0e2a7a83",
+    verifying_contract: "0xc673e4ffcb8464faff908a6804fe0e635af0ea2f",
+    version: "1"
+  ]
