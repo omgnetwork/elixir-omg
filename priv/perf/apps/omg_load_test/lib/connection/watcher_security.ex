bc42495645652f0e45dcc397b6de5a87e904a514
Piotr Żelazko <piotr.zelazko@imapp.pl>
2020-03-10 16:50:07 +0100

    fix: change json serdes backend

--- priv/perf/apps/omg_load_test/lib/connection/watcher_security.ex
+++ priv/perf/apps/omg_load_test/lib/connection/watcher_security.ex
@@ -25,7 +25,7 @@ defmodule OMG.LoadTest.Connection.WatcherSecurity do
     middleware = [
       Tesla.Middleware.Logger,
       {Tesla.Middleware.BaseUrl, base_url},
-      {Tesla.Middleware.EncodeJson, engine: Jason},
+      {Tesla.Middleware.EncodeJson, engine: Poison},
       {Tesla.Middleware.Headers, [{"user-agent", "Perf"}]},
       {Tesla.Middleware.Retry, delay: 500, max_retries: 10, max_delay: 45_000, should_retry: Utils.retry?()},
       {Tesla.Middleware.Timeout, timeout: 30_000},
