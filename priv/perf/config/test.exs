b7d4eac8e4a0e2922750c1d9a53f2d9ca83f94ba
Paweł Gębal <pawel.gebal@omisego.co>
2020-03-24 10:58:48 +0100

    refactor: move hardcoded contract addresses, private keys out of configs for test env

--- priv/perf/config/test.exs
+++ priv/perf/config/test.exs
@@ -5,21 +5,19 @@ config :ethereumex,
 
 config :load_test,
   faucet_deposit_wei: trunc(:math.pow(10, 18) * 10),
-  faucet_private_key:
-    System.get_env("LOAD_TEST_FAUCET_PRIVATE_KEY") ||
-      "0xd885a307e35738f773d8c9c63c7a3f3977819274638d04aaf934a1e1158513ce",
+  faucet_private_key: System.get_env("LOAD_TEST_FAUCET_PRIVATE_KEY"),
   child_chain_url: System.get_env("CHILD_CHAIN_URL") || "http://localhost:9656",
   fee_wei: 1,
   utxo_load_test_config: %{
     utxos_to_create_per_session: 1_000,
     transactions_per_session: 100
   },
-  eth_vault_address: "0x4e3aeff70f022a6d4cc5947423887e7152826cf7"
+  eth_vault_address: System.get_env("CONTRACT_ADDRESS_ETH_VAULT")
 
 config :ex_plasma,
   eip_712_domain: [
     name: "OMG Network",
     salt: "0xfad5c7f626d80f9256ef01929f3beb96e058b8b4b0e3fe52d84f054c0e2a7a83",
-    verifying_contract: "0xc673e4ffcb8464faff908a6804fe0e635af0ea2f",
+    verifying_contract: System.get_env("CONTRACT_ADDRESS_PLASMA_FRAMEWORK"),
     version: "1"
   ]
