e96ac4e86bf43cf62f88e18028178355321fb2d1
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2020-03-31 23:05:47 +0700

    test: add fee_specs.json for testing

--- apps/omg_child_chain/config/test.exs
+++ apps/omg_child_chain/config/test.exs
@@ -3,4 +3,9 @@ use Mix.Config
 config :omg_child_chain,
   block_queue_eth_height_check_interval_ms: 100,
   fee_adapter_check_interval_ms: 1_000,
-  fee_buffer_duration_ms: 5_000
+  fee_buffer_duration_ms: 5_000,
+  fee_adapter:
+    {OMG.ChildChain.Fees.FileAdapter,
+     opts: [
+       specs_file_path: Path.join(Path.dirname(__DIR__), "test/support/fee_specs.json")
+     ]}
