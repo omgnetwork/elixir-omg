f2ba5e0bd79cca2713ad8bdd5b4cb5325dfcdd93
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2020-04-22 14:58:48 +0700

    feat: system memory monitor that considers buffered and cached memory (#1474)
    * refactor: move OMG.Status.Application config retrieval into OMG.Status.Configuration
    
    * Move StatsdMonitor from Metric to Monitor namespace
    
    * feat: add OMG.Status.Monitor.MemoryMonitor
    
    * feat: add memory monitor configs
    
    * feat: add system memeory high threshold config
    
    * test: add tests for MemoryMonitor
    
    * refactor: rename tref to timer_ref
    
    * refactor: consistently use Application.fetch_env! in OMG.Status.Configuration

---
+++ apps/omg_status/lib/omg_status/configuration.ex
@@ -0,0 +1,37 @@
+# Copyright 2019-2020 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.Status.Configuration do
+  @moduledoc """
+  Provides access to applications configuration
+  """
+  alias OMG.Status.Metric.Tracer
+
+  @app :omg_status
+
+  @spec system_memory_check_interval_ms() :: integer() | no_return()
+  def system_memory_check_interval_ms() do
+    Application.fetch_env!(@app, :system_memory_check_interval_ms)
+  end
+
+  @spec system_memory_high_threshold() :: float() | no_return()
+  def system_memory_high_threshold() do
+    Application.fetch_env!(@app, :system_memory_high_threshold)
+  end
+
+  @spec datadog_disabled?() :: boolean()
+  def datadog_disabled?() do
+    Application.fetch_env!(@app, Tracer)[:disabled?]
+  end
+end
