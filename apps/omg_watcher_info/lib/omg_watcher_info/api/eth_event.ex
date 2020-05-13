bc9675de5f83df2404a69cb767de4b0c81bf5bff
okalouti <omar@omisego.co>
2020-05-13 11:13:40 +0200

    add: api-level code for deposit.all endpoint

---
+++ apps/omg_watcher_info/lib/omg_watcher_info/api/eth_event.ex
@@ -0,0 +1,38 @@
+# Copyright 2019-2020 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.WatcherInfo.API.EthEvent do
+  @moduledoc """
+  Module provides operations related to Ethereum events - i.e. deposits and exits.
+  """
+
+  alias OMG.Utils.Paginator
+  alias OMG.WatcherInfo.DB
+
+  @default_events_limit 100
+
+  @doc """
+  Retrieves a list of deposits.
+  Length of the list is limited by `limit` and `page` arguments.
+  Optionally filtered by `address`
+  """
+  @spec get_deposits(Keyword.t()) :: Paginator.t(%DB.EthEvent{})
+  def get_deposits(constraints) do
+    address = Keyword.get(constraints, :address)
+
+    constraints
+    |> Paginator.from_constraints(@default_events_limit)
+    |> DB.EthEvent.get_events(:deposit, address)
+  end
+end
