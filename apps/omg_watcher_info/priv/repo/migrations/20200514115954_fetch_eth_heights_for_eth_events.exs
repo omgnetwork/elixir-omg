a3150871c74dd4f1b66a39356706e88fc5b42d64
okalouti <omar@omisego.co>
2020-05-16 19:02:50 +0200

    refactor and add: supporting code for migration

--- apps/omg_watcher_info/priv/repo/migrations/20200514115954_fetch_eth_heights_for_eth_events.exs
+++ apps/omg_watcher_info/priv/repo/migrations/20200514115954_fetch_eth_heights_for_eth_events.exs
@@ -1,29 +1,42 @@
 defmodule OMG.WatcherInfo.DB.Repo.Migrations.FetchEthHeightsForEthEvents do
   use Ecto.Migration
-
   alias OMG.Eth.Encoding
   alias OMG.WatcherInfo.DB
 
-  def change do
+  import Ecto.Query, only: [from: 2]
+
+  def up() do
+    nil
+    # request_block_numbers()
+    # |> format_responses()
+    # |> Enum.each(&update_record/1)
   end
 
   defp request_block_numbers do
     {:ok, batched_responses} =
-      from(event in DB.EthEvent, select: event)
+      from(e in DB.EthEvent,
+        where: is_nil(e.eth_height),
+        select: e.root_chain_txhash
+      )
       |> DB.Repo.all()
-      |> Enum.map(&create_request_from_event/1)
-      |> Ethereumex.HttpClient.batch_request()
+      |> Enum.map(&create_request/1)
+      |> make_batched_request()
 
     batched_responses
   end
 
-  defp create_request_from_event(event) do
-    hex_hash =
-      event
-      |> Map.get(:root_chain_txhash)
-      |> Encoding.to_hex()
+  defp create_request(root_chain_txhash) do
+    {:eth_get_transaction_by_hash, [Encoding.to_hex(root_chain_txhash)]}
+  end
 
-    {:eth_get_transaction_by_hash, [hex_hash]}
+  defp make_batched_request(requests) do
+    case requests do
+      [] ->
+        {:ok, []}
+
+      _ ->
+        Ethereumex.HttpClient.batch_request(requests)
+    end
   end
 
   defp format_responses(res) do
@@ -31,10 +44,39 @@ defmodule OMG.WatcherInfo.DB.Repo.Migrations.FetchEthHeightsForEthEvents do
       eth_height =
         event
         |> Map.get("blockNumber")
+        |> normalize_hash()
         |> Encoding.from_hex()
         |> :binary.decode_unsigned()
 
-      %{root_chain_txhash: event["hash"], eth_height: eth_height}
+      root_chain_txhash =
+        event
+        |> Map.get("hash")
+        |> Encoding.from_hex()
+
+      %{root_chain_txhash: root_chain_txhash, eth_height: eth_height}
+    end)
+  end
+
+  def normalize_hash("0x" <> hex = hash) do
+    case hex |> String.length() |> rem(2) do
+      0 ->
+        hash
+
+      _ ->
+        "0x0" <> hex
+    end
+  end
+
+  defp update_record(%{root_chain_txhash: root_chain_txhash, eth_height: eth_height} = _response) do
+    from(e in DB.EthEvent,
+      where: e.root_chain_txhash == ^root_chain_txhash,
+      select: e
+    )
+    |> DB.Repo.all()
+    |> Enum.each(fn event ->
+      event
+      |> Ecto.Changeset.change(%{eth_height: eth_height})
+      |> DB.Repo.update()
     end)
   end
 end
