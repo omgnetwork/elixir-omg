1ba1a3deec0396e1dba547a6f78d44459d3a3fb0
souradeep-das <dsouradeep2@gmail.com>
2020-05-18 22:14:41 +0530

    revert to eth_height_now

--- apps/omg_watcher/lib/omg_watcher/exit_processor.ex
+++ apps/omg_watcher/lib/omg_watcher/exit_processor.ex
@@ -610,7 +610,7 @@ defmodule OMG.Watcher.ExitProcessor do
   end
 
   defp run_status_gets(%ExitProcessor.Request{eth_height_now: nil, blknum_now: nil} = request) do
-    {:ok, eth_height_now} = Eth.get_block_timestamp_by_number(EthereumHeight.get())
+    {:ok, eth_height_now} = EthereumHeight.get()
     {blknum_now, _} = State.get_status()
 
     _ = Logger.debug("eth_height_now: #{inspect(eth_height_now)}, blknum_now: #{inspect(blknum_now)}")
