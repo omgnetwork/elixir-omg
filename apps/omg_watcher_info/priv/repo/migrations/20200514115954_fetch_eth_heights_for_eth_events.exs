78a4d0d177b9dedcc131e9906ca3245812071dde
okalouti <omar@omisego.co>
2020-05-19 15:12:00 +0200

    refactor: use Repo.stream

--- apps/omg_watcher_info/priv/repo/migrations/20200514115954_fetch_eth_heights_for_eth_events.exs
+++ apps/omg_watcher_info/priv/repo/migrations/20200514115954_fetch_eth_heights_for_eth_events.exs
@@ -7,8 +7,8 @@ defmodule OMG.WatcherInfo.DB.Repo.Migrations.FetchEthHeightsForEthEvents do
 
   def up() do
     request_block_numbers()
-    |> format_responses()
-    |> Stream.each(&update_record/1)
+    |> Enum.map(&format_response/1)
+    |> Enum.each(&update_record/1)
   end
 
   defp request_block_numbers() do
@@ -17,7 +17,7 @@ defmodule OMG.WatcherInfo.DB.Repo.Migrations.FetchEthHeightsForEthEvents do
         where: is_nil(e.eth_height),
         select: e.root_chain_txhash
       )
-      |> DB.Repo.all()
+      |> DB.Repo.stream(max_rows: 100)
       |> Enum.map(&create_request/1)
       |> make_batched_request()
 
@@ -28,32 +28,30 @@ defmodule OMG.WatcherInfo.DB.Repo.Migrations.FetchEthHeightsForEthEvents do
     {:eth_get_transaction_by_hash, [Encoding.to_hex(root_chain_txhash)]}
   end
 
-  defp make_batched_request(requests) do
-    case requests do
+  defp make_batched_request(eth_requests) do
+    case eth_requests do
       [] ->
         {:ok, []}
 
       _ ->
-        Ethereumex.HttpClient.batch_request(requests)
+        Ethereumex.HttpClient.batch_request(eth_requests)
     end
   end
 
-  defp format_responses(res) do
-    Stream.map(res, fn event ->
-      eth_height =
-        event
-        |> Map.get("blockNumber")
-        |> normalize_hash()
-        |> Encoding.from_hex()
-        |> :binary.decode_unsigned()
+  defp format_response(event) do
+    eth_height =
+      event
+      |> Map.get("blockNumber")
+      |> normalize_hash()
+      |> Encoding.from_hex()
+      |> :binary.decode_unsigned()
 
-      root_chain_txhash =
-        event
-        |> Map.get("hash")
-        |> Encoding.from_hex()
+    root_chain_txhash =
+      event
+      |> Map.get("hash")
+      |> Encoding.from_hex()
 
-      %{root_chain_txhash: root_chain_txhash, eth_height: eth_height}
-    end)
+    %{root_chain_txhash: root_chain_txhash, eth_height: eth_height}
   end
 
   def normalize_hash("0x" <> hex = hash) do
