4561edaf73d3cc0ee2441ffa9624cf082604797a
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2020-05-21 01:47:32 +0700

    debug: is barebone test failing just because of perf?

--- priv/cabbage/apps/itest/lib/poller.ex
+++ priv/cabbage/apps/itest/lib/poller.ex
@@ -86,7 +86,7 @@ defmodule Itest.Poller do
   @doc """
   API:: Pull until the utxo is not found for the address.
   """
-  def utxo_absent?(address, utxo_pos), do: utxo_absent?(address, utxo_pos, @retry_count)
+  def utxo_absent?(address, utxo_pos), do: utxo_absent?(address, utxo_pos, 300)
 
   @doc """
   API:: Pull until the exitable utxo is not found for the address.
