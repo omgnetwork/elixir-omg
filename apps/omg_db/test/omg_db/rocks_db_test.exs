dd5f33de10b7f34d334f8047954b7b1db7f9a13d
boolafish <boolafish945@gmail.com>
2020-05-22 17:49:21 +0900

    refactor: move exit info related DB functions to PaymentExitInfo (#1503)
    * feat: add get and get_all_by_type in rocksdb.server
    
    * feat: behaviour for PaymentExitInfo
    
    * feat: rocksdb impl of PaymentExitInfo for exit_infos
    
    * feat: impl of in_flight_exits_info
    
    * refactor: remove unused 'type' in decode_value(s)
    
    * feat: exit_info impl
    
    * tests: PaymentExitInfo db test
    
    * cleanup: remove exit info related codes from old DB modules and tests
    
    * fix: use PaymentExitInfo instead of DB
    
    * fix: behaviour module with driver
    
    * fix: make linter happy :)
    
    * chore: add @impl flag for compile warning
    
    * chore: add get_single_value to @callback list
    
    * refactor: restrucure PaymentExitInfo to call OMG.DB directly
    
    So instead of calling the rock db implementation, it calls the DB interface.
    This removes the need of having the a PaymentExitInfo behaviour.
    
    * test: unit test for get and get_all_by_type function
    
    * fix: default @server_name value in PaymentExitInfo
    
    * fix: rebase
    
    * feat: DB.get with list of specific keys instead
    
    Receiving list of specific keys is more useful :)
    Also make changes to the PaymentExitInfo apis accordingly
    
    * feat: generic DB function with timeout
    
    * refactor: rename function and add optional args for timeout
    
    Use keyword list to support multi optional args. (timeout and server)
    
    * refactor: rename in_flight_exit_info() -> all_in_flight_exit_infos()

--- apps/omg_db/test/omg_db/rocks_db_test.exs
+++ apps/omg_db/test/omg_db/rocks_db_test.exs
@@ -78,11 +78,34 @@ defmodule OMG.RocksDBTest do
     {:ok, ["xyz", "vxyz", "wvxyz"]} = OMG.DB.block_hashes([1, 2, 3], pid)
   end
 
-  test "if multi reading exit infos returns writen results", %{db_dir: _dir, db_pid: pid} do
-    db_writes = create_write(:exit_info, pid)
-    {:ok, exits} = DB.exit_infos(pid)
-    # what we wrote and what we read must be equal
-    [] = exits -- db_writes
+  describe "batch_get" do
+    test "can get single data with the type and single specific key", %{db_dir: _dir, db_pid: pid} do
+      type = :exit_info
+      specific_key = {1, 1, 1}
+      data = {specific_key, :crypto.strong_rand_bytes(123)}
+      :ok = DB.multi_update([{:put, type, data}], pid)
+
+      assert {:ok, [data]} == DB.batch_get(type, [specific_key], server: pid)
+    end
+
+    test "can get multiple data with the type and multiple specific keys", %{db_dir: _dir, db_pid: pid} do
+      type = :exit_info
+      specific_keys = [{1, 1, 1}, {2, 2, 2}]
+      data_list = Enum.map(specific_keys, fn key -> {key, :crypto.strong_rand_bytes(123)} end)
+
+      :ok =
+        data_list
+        |> Enum.map(fn data -> {:put, type, data} end)
+        |> DB.multi_update(pid)
+
+      assert {:ok, data_list} == DB.batch_get(type, specific_keys, server: pid)
+    end
+  end
+
+  test "it can get all data with the type", %{db_dir: _dir, db_pid: pid} do
+    db_writes = create_write(:utxo, pid)
+
+    assert {:ok, db_writes} == DB.get_all_by_type(:utxo, server: pid)
   end
 
   test "if multi reading utxos returns writen results", %{db_dir: _dir, db_pid: pid} do
@@ -91,46 +114,10 @@ defmodule OMG.RocksDBTest do
     [] = utxos -- db_writes
   end
 
-  test "if multi reading in flight exit infos returns writen results", %{db_dir: _dir, db_pid: pid} do
-    db_writes = create_write(:in_flight_exit_info, pid)
-    {:ok, in_flight_exits_infos} = DB.in_flight_exits_info(pid)
-    [] = in_flight_exits_infos -- db_writes
-  end
-
   test "if multi reading competitor infos returns writen results", %{db_dir: _dir, db_pid: pid} do
     db_writes = create_write(:competitor_info, pid)
     {:ok, competitors_info} = DB.competitors_info(pid)
     [] = competitors_info -- db_writes
-  end
-
-  test "if multi reading and writting does not pollute returned values", %{db_dir: _dir, db_pid: pid} do
-    db_writes = create_write(:exit_info, pid)
-    {:ok, exits} = DB.exit_infos(pid)
-    # what we wrote and what we read must be equal
-    [] = exits -- db_writes
-
-    db_writes = create_write(:utxo, pid)
-    {:ok, utxos} = DB.utxos(pid)
-    # what we wrote and what we read must be equal
-    [] = utxos -- db_writes
-
-    db_writes = create_write(:in_flight_exit_info, pid)
-    {:ok, in_flight_exits_infos} = DB.in_flight_exits_info(pid)
-    # what we wrote and what we read must be equal
-    [] = in_flight_exits_infos -- db_writes
-
-    db_writes = create_write(:competitor_info, pid)
-    {:ok, competitors_info} = DB.competitors_info(pid)
-    # what we wrote and what we read must be equal
-    [] = competitors_info -- db_writes
-  end
-
-  defp create_write(:exit_info = type, pid) do
-    db_writes =
-      Enum.map(1..@writes, fn index -> {:put, type, {{index, index, index}, :crypto.strong_rand_bytes(index)}} end)
-
-    :ok = write(db_writes, pid)
-    get_raw_values(db_writes)
   end
 
   defp create_write(:utxo = type, pid) do
@@ -138,13 +125,6 @@ defmodule OMG.RocksDBTest do
       Enum.map(1..@writes, fn index ->
         {:put, type, {{index, index, index}, %{test: :crypto.strong_rand_bytes(index)}}}
       end)
-
-    :ok = write(db_writes, pid)
-    get_raw_values(db_writes)
-  end
-
-  defp create_write(:in_flight_exit_info = type, pid) do
-    db_writes = Enum.map(1..@writes, fn index -> {:put, type, {:crypto.strong_rand_bytes(index), index}} end)
 
     :ok = write(db_writes, pid)
     get_raw_values(db_writes)
