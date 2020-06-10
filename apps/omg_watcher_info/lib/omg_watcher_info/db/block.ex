a602b739b552c0184a5d65b00cdc16cd9257b6ea
Mederic <mederic@omise.co>
2020-06-10 11:48:38 +0700

    sobelow skip BinToTerm

--- apps/omg_watcher_info/lib/omg_watcher_info/db/block.ex
+++ apps/omg_watcher_info/lib/omg_watcher_info/db/block.ex
@@ -145,6 +145,7 @@ defmodule OMG.WatcherInfo.DB.Block do
   @doc """
   Takes a pending block, decode its data and inserts its content in the database.
   """
+  # sobelow_skip ["Misc.BinToTerm"]
   @spec insert_pending_block(PendingBlock.t()) :: {:ok, %__MODULE__{}} | {:error, any()}
   def insert_pending_block(pending_block) do
     %{
