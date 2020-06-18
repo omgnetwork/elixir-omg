2dfbd5801b9bab002bafe1aa1443a101744984df
souradeep-das <dsouradeep2@gmail.com>
2020-06-18 20:36:07 +0530

    update deps with  ex_plasma

--- priv/perf/apps/load_test/mix.exs
+++ priv/perf/apps/load_test/mix.exs
@@ -33,7 +33,7 @@ defmodule LoadTest.MixProject do
       {:chaperon, "~> 0.3.1"},
       {:tesla, "~> 1.3.0"},
       {:httpoison, "~> 1.6.2", override: true},
-      {:ex_plasma, git: "https://github.com/omisego/ex_plasma.git", override: true},
+      {:ex_plasma, git: "https://github.com/omisego/ex_plasma.git", branch: "master-v2", override: true},
 
       # Better adapter for tesla
       {:hackney, "~> 1.15.2"},
