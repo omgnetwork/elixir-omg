c93f560d5c315eaf5a47603eee1a89e1906ca445
jarindr <jarindr23@gmail.com>
2020-06-23 17:33:50 +0700

    mix format

--- apps/omg_watcher_rpc/test/omg_watcher_rpc/web/validators/transaction_constraints_test.exs
+++ apps/omg_watcher_rpc/test/omg_watcher_rpc/web/validators/transaction_constraints_test.exs
@@ -35,11 +35,18 @@ defmodule OMG.WatcherRPC.Web.Validator.TransactionConstraintsTest do
         "blknum" => 1000,
         "metadata" => Encoding.to_hex(@zero_metadata),
         "txtypes" => [1, 3],
-        "end_datetime" => 12345678
+        "end_datetime" => 12_345_678
       }
 
       {:ok, constraints} = TransactionConstraints.parse(request_data)
-      assert constraints == [txtypes: [1, 3], metadata: @zero_metadata, blknum: 1000, address: @eth, end_datetime: 12345678]
+
+      assert constraints == [
+               txtypes: [1, 3],
+               metadata: @zero_metadata,
+               blknum: 1000,
+               address: @eth,
+               end_datetime: 12_345_678
+             ]
     end
 
     test "filters unsupported constraints" do
@@ -95,8 +102,11 @@ defmodule OMG.WatcherRPC.Web.Validator.TransactionConstraintsTest do
     end
 
     test "returns a :validation_error when the given end_datetime is not an integer" do
-      assert TransactionConstraints.parse(%{"end_datetime" => 3.14}) == {:error, {:validation_error, "end_datetime", :integer}}
-      assert TransactionConstraints.parse(%{"end_datetime" => "abcd"}) == {:error, {:validation_error, "end_datetime", :integer}}
+      assert TransactionConstraints.parse(%{"end_datetime" => 3.14}) ==
+               {:error, {:validation_error, "end_datetime", :integer}}
+
+      assert TransactionConstraints.parse(%{"end_datetime" => "abcd"}) ==
+               {:error, {:validation_error, "end_datetime", :integer}}
     end
   end
 end
