ceff31520b17a60e3c0f05bdbef22bd987849b96
boolafish <boolafish945@gmail.com>
2020-06-30 16:45:06 +0900

    test: payment v2 with standard exit (#1589)
    * feat: multiple exit processors (#1573)
    
    * feat: start multiple exit processors
    
    * chore: use snapshot support payment v2
    
    ... and run mix format
    
    * fix: setup exit_games for test fixture
    
    * fix: dispatch return in the format of {:ok, []}
    
    * fix: make watcher integ test to work
    
    * fix: unit test for EthEventAggregator
    
    1. fix the unit tests, add address to log result
    2. change how we bind contract address to log, use the pre-decoded address data
    
    * fix: patch payment_exit_game back into contract_addr
    
    * fix: wrong filter_events mapping
    
    * fix: linter, dializer, remove tmp logging
    
    * fix: use snapwhot with payment v2
    
    Previous snapshot was wrong.
    Using this one:
    https://github.com/omisego-images/docker-elixir-omg/pull/43#issuecomment-620469853
    
    * refactor: restrain from using ++
    
    Use [a|b] as best practice
    
    * chore: use Task.async_stream instad of sync looping
    
    * refactor: move private functions to the end
    
    * fix: map after Task.async_stream
    
    Co-authored-by: Thibault Denizet <thibault@omisego.co>
    
    * feat: test with payment v2 transfer
    
    Tested by hacking the watcher info /transaction.create to generate payment v2 tx instead.
    And then the tx would be signed and send normally. Tested with the cabbage transaction tests.
    
    This commit instead of providing new module for payment, it reuses most of the part instead of the function
    that generate the payment struct. This commit introduces Payment.Builder to build different payment tx.
    
    Probably not the most clean way of handling things but the least change required to allow cabbage test to work.
    
    * feat: allow /transaction.create to pass in tx_type
    
    add tx_type as a param of 'order'. If not passed, default to 1 for backward competibility.
    
    * test: cabbage test for payment v2 transfer
    
    * fix: cleanup
    
    linter, dialyzer, remove branching on tx type when building payment tx.
    
    * refactor: use Alice and Bob instead
    
    Simplify the test code and make it easier to read
    
    * fix: credo complain single pipe
    
    * test: payment v2 with standard exit
    
    * fix: compiler warning me :(
    
    * refactor: remove commented codes
    
    * fix: linter
    
    * style: simpler code
    
    * refactor: parameterize users
    
    Co-authored-by: Thibault Denizet <thibault@omisego.co>

--- priv/cabbage/apps/itest/test/itest/payment_v2_test.exs
+++ priv/cabbage/apps/itest/test/itest/payment_v2_test.exs
@@ -18,83 +18,96 @@ defmodule PaymentV2Test do
   alias Itest.Account
 
   alias Itest.Client
+  alias Itest.StandardExitClient
   alias Itest.Transactions.Currency
 
   @payment_v2_tx_type 2
 
-  setup do
-    [alice, bob] =
-      Enum.map(
-        Account.take_accounts(2),
-        fn {account, pkey} -> %{account: account, pkey: pkey} end
-      )
-
-    %{alice: alice, bob: bob}
+  defgiven ~r/^(?<user>[\w]+) has an ethereum account$/, %{user: user}, state do
+    [{account, pkey}] = Account.take_accounts(1)
+    {:ok, Map.put(state, user, %{account: account, pkey: pkey})}
   end
 
-  defwhen ~r/^Alice deposits "(?<amount>[^"]+)" ETH to the root chain$/,
-          %{amount: amount},
-          %{alice: alice} = state do
+  defwhen ~r/^(?<user>[\w]+) deposits "(?<amount>[^"]+)" ETH to the root chain$/,
+          %{user: user, amount: amount},
+          state do
+    user = state[user]
+
     {:ok, _receipt_hash} =
       amount
       |> Currency.to_wei()
-      |> Client.deposit(alice.account, Itest.PlasmaFramework.vault(Currency.ether()))
+      |> Client.deposit(user.account, Itest.PlasmaFramework.vault(Currency.ether()))
 
     {:ok, state}
   end
 
-  defthen ~r/^Alice should have "(?<amount>[^"]+)" ETH on the child chain$/,
-          %{amount: amount},
-          %{alice: alice} = state do
+  defthen ~r/^(?<user>[\w]+) should have "(?<amount>[^"]+)" ETH on the child chain$/,
+          %{user: user, amount: amount},
+          state do
+    user = state[user]
     expecting_amount = Currency.to_wei(amount)
-    balance = Client.get_exact_balance(alice.account, expecting_amount)
-    balance = balance["amount"]
+    balance = Client.get_exact_balance(user.account, expecting_amount)["amount"] || 0
 
-    assert_equal(expecting_amount, balance, "For #{alice.account}")
+    assert_equal(expecting_amount, balance, "For #{user.account}")
 
     {:ok, state}
   end
 
-  defwhen ~r/^Alice sends Bob "(?<amount>[^"]+)" ETH on the child chain with payment v1$/,
-          %{amount: amount},
-          %{alice: alice, bob: bob} = state do
+  defwhen ~r/^(?<sender>[\w]+) sends (?<receiver>[\w]+) "(?<amount>[^"]+)" ETH on the child chain with payment v1$/,
+          %{sender: sender, receiver: receiver, amount: amount},
+          state do
+    sender = state[sender]
+    receiver = state[receiver]
+
     {:ok, [sign_hash, typed_data, _txbytes]} =
       Client.create_transaction(
         Currency.to_wei(amount),
-        alice.account,
-        bob.account
+        sender.account,
+        receiver.account
       )
 
-    _ = Client.submit_transaction(typed_data, sign_hash, [alice.pkey])
+    _ = Client.submit_transaction(typed_data, sign_hash, [sender.pkey])
 
     {:ok, state}
   end
 
-  defwhen ~r/^Alice sends Bob "(?<amount>[^"]+)" ETH on the child chain with payment v2$/,
-          %{amount: amount},
-          %{alice: alice, bob: bob} = state do
+  defwhen ~r/^(?<sender>[\w]+) sends (?<receiver>[\w]+) "(?<amount>[^"]+)" ETH on the child chain with payment v2$/,
+          %{sender: sender, receiver: receiver, amount: amount},
+          state do
+    sender = state[sender]
+    receiver = state[receiver]
+
     {:ok, [sign_hash, typed_data, _txbytes]} =
       Client.create_transaction(
         Currency.to_wei(amount),
-        alice.account,
-        bob.account,
+        sender.account,
+        receiver.account,
         Currency.ether(),
         @payment_v2_tx_type
       )
 
-    _ = Client.submit_transaction(typed_data, sign_hash, [alice.pkey])
+    _ = Client.submit_transaction(typed_data, sign_hash, [sender.pkey])
 
     {:ok, state}
   end
 
-  defthen ~r/^Bob should have "(?<amount>[^"]+)" ETH on the child chain$/,
-          %{amount: amount},
-          %{bob: bob} = state do
-    expecting_amount = Currency.to_wei(amount)
-    balance = Client.get_exact_balance(bob.account, expecting_amount)["amount"]
+  defwhen ~r/^(?<user>[\w]+) starts a standard exit with the payment v2 output$/, %{user: user}, state do
+    user = state[user]
+    se = StandardExitClient.start_standard_exit(user.account, @payment_v2_tx_type)
+    state = Map.put_new(state, :standard_exit, se)
 
-    assert_equal(expecting_amount, balance, "For Bob: #{bob.account}")
+    {:ok, state}
+  end
 
+  defthen ~r/^(?<user>[\w]+) should no longer see the exiting utxo on the child chain$/, %{user: user}, state do
+    user = state[user]
+    assert Itest.Poller.utxo_absent?(user.account, state.standard_exit.utxo.utxo_pos)
+    assert Itest.Poller.exitable_utxo_absent?(user.account, state.standard_exit.utxo.utxo_pos)
+    {:ok, state}
+  end
+
+  defwhen ~r/^Somebody processes the standard exit on the child chain$/, _, state do
+    StandardExitClient.wait_and_process_standard_exit(state.standard_exit)
     {:ok, state}
   end
 
