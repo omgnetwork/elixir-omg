2c0375b7abd58a96aeeb6935e2f5bbc413778e4e
Mederic <mederic@omise.co>
2020-07-21 15:08:02 +0700

    add missing clause on witness validation check

---
+++ apps/omg/test/omg/state/transaction/witness_test.exs
@@ -0,0 +1,35 @@
+# Copyright 2019-2020 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.State.Transaction.WitnessTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  alias OMG.State.Transaction.Witness
+
+  describe "valid?/1" do
+    test "returns true when is binary and 65 bytes long" do
+      assert Witness.valid?(<<0::520>>) == true
+    end
+
+    test "returns false when not a binary" do
+      assert Witness.valid?([<<0>>]) == false
+    end
+
+    test "returns false when not 65 bytes long" do
+      assert Witness.valid?(<<0>>) == false
+    end
+  end
+end
