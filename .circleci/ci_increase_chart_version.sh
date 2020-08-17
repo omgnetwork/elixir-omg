822739cae70b86e7acbc64bcf0c9d0a206a86ee4
boolafish <boolafish945@gmail.com>
2020-08-17 18:30:05 +0900

    fix: make increase chart script work

--- .circleci/ci_increase_chart_version.sh
+++ .circleci/ci_increase_chart_version.sh
@@ -12,25 +12,18 @@ Required env vars:
 """
 
 set -e
+set -x
 
 increase_chart_version() {
     APP_VERSION="${CIRCLE_TAG#*v}"
 
-    curl --location --request POST 'https://api.github.com/repos/omgnetwork/${HELM_CHART_REPO}/dispatches' \
+    echo "increase chart version for chart: ${CHART_NAME} with APP_VERSION: ${APP_VERSION}"
+
+    curl --location --request POST 'https://api.github.com/repos/omgnetwork/'${HELM_CHART_REPO}'/dispatches' \
     --header 'Accept: application/vnd.github.v3+json' \
-    --header 'authorization: token ${GITHUB_API_TOKEN}' \
+    --header 'authorization: token '${GITHUB_API_TOKEN}'' \
     --header 'Content-Type: application/json' \
-    --data-raw '{"event_type": "increase-chart-version", "client_payload": { "chart_name": ${CHART_NAME}, "app_version": ${APP_VERSION} }}'
+    --data-raw '{"event_type": "increase-chart-version", "client_payload": { "chart_name": "'${CHART_NAME}'", "app_version": "'${APP_VERSION}'" }}'
 }
 
-if [[ -n "$CIRCLE_TAG" ]]; then
-    # if the tag start with a version. eg. `v1.0.3-pre.0`
-    # otherwise it is not a release tag
-    if [[ $CIRCLE_TAG =~ ^v.* ]]; then
-        increase_chart_version
-    else
-        echo "Not tag for release version, skipping increase chart version..."
-    fi
-else
-    echo "There is no circle CI tag, skipping increase chart version...."
-fi
+increase_chart_version
