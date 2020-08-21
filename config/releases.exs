e77ed9a256073a6e7a13d2ce7f6063b18f356cb2
unnawut <unnawut@omg.network>
2020-08-21 14:41:38 +0700

    refactor: move max gas price and gas price strategy to releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -5,6 +5,17 @@ import Config
 #
 # See https://hexdocs.pm/mix/1.9.0/Mix.Tasks.Release.html#module-runtime-configuration
 
+config :omg_child_chain,
+  block_submit_max_gas_price: String.to_integer(System.get_env("BLOCK_SUBMIT_MAX_GAS_PRICE") || "20000000000"),
+  block_submit_gas_price_strategy:
+    case System.get_env("BLOCK_SUBMIT_GAS_PRICE_STRATEGY") do
+      "POISSON" -> OMG.ChildChain.GasPrice.Strategy.PoissonGasStrategy
+      "BLOCK_PERCENTILE" -> OMG.ChildChain.GasPrice.Strategy.BlockPercentileGasStrategy
+      "LEGACY" -> OMG.ChildChain.GasPrice.Strategy.LegacyGasStrategy
+      nil -> OMG.ChildChain.GasPrice.Strategy.LegacyGasStrategy
+      invalid -> raise("Invalid gas strategy. Got: #{invalid}")
+    end
+
 config :omg_watcher_info, OMG.WatcherInfo.DB.Repo,
   # Have at most `:pool_size` DB connections on standby and serving DB queries.
   pool_size: String.to_integer(System.get_env("WATCHER_INFO_DB_POOL_SIZE") || "10"),
