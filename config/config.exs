f62dc065d01e297508fff8a6ca35f5a72c0e9e3a
Ayrat Badykov <ayratin555@gmail.com>
2020-08-28 22:33:08 +0300

    move omg_performance json rpc tests to perf project (#1691)

--- config/config.exs
+++ config/config.exs
@@ -232,7 +232,4 @@ config :spandex_phoenix, tracer: OMG.WatcherRPC.Tracer
 
 config :briefly, directory: ["/tmp/omisego"]
 
-# `watcher_url` must match respective `:omg_watcher_rpc, OMG.WatcherRPC.Web.Endpoint`
-config :omg_performance, watcher_url: "localhost:7434"
-
 import_config "#{Mix.env()}.exs"
--- priv/perf/apps/load_test/lib/common/generators.ex
+++ priv/perf/apps/load_test/lib/common/generators.ex
@@ -12,19 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-defmodule OMG.Performance.Generators do
+defmodule LoadTest.Common.Generators do
   @moduledoc """
   Provides helper functions to generate bundles of various useful entities for performance tests
   """
-  require OMG.Utxo
 
-  alias OMG.Eth.Configuration
-  alias OMG.Eth.RootChain
-
-  alias OMG.State.Transaction
-  alias OMG.Utxo
-  alias OMG.Watcher.HttpRPC.Client
-  alias Support.DevHelper
+  alias ExPlasma.Encoding
+  alias LoadTest.ChildChain.Transaction
+  alias LoadTest.Ethereum
+  alias LoadTest.Ethereum.Account
 
   @generate_user_timeout 600_000
 
@@ -35,10 +31,10 @@ defmodule OMG.Performance.Generators do
     - :faucet - the address to send the test ETH from, assumed to be unlocked and have the necessary funds
     - :initial_funds_wei - the amount of test ETH that will be granted to every generated user
   """
-  @spec generate_users(non_neg_integer, [Keyword.t()]) :: [OMG.TestHelper.entity()]
-  def generate_users(size, opts \\ []) do
+  @spec generate_users(non_neg_integer) :: [map()]
+  def generate_users(size) do
     1..size
-    |> Task.async_stream(fn _ -> generate_user(opts) end, timeout: @generate_user_timeout)
+    |> Task.async_stream(fn _ -> generate_user() end, timeout: @generate_user_timeout)
     |> Enum.map(fn {:ok, result} -> result end)
   end
 
@@ -60,10 +56,10 @@ defmodule OMG.Performance.Generators do
     if opts[:take], do: Enum.take(utxo_positions, opts[:take]), else: utxo_positions
   end
 
-  @spec stream_blocks() :: [OMG.Block.t()]
+  @spec stream_blocks() :: [map()]
   defp stream_blocks() do
-    child_chain_url = OMG.Watcher.Configuration.child_chain_url()
-    interval = Configuration.child_block_interval()
+    child_chain_url = Application.fetch_env!(:load_test, :child_chain_url)
+    interval = Application.fetch_env!(:load_test, :child_block_interval)
 
     Stream.map(
       Stream.iterate(1, &(&1 + 1)),
@@ -71,23 +67,27 @@ defmodule OMG.Performance.Generators do
     )
   end
 
-  defp generate_user(opts) do
-    user = OMG.TestHelper.generate_entity()
-    {:ok, _user} = DevHelper.import_unlock_fund(user, opts)
+  defp generate_user() do
+    {:ok, user} = Account.new()
+
+    {:ok, address} = Ethereum.create_account_from_secret(user.priv, "pass")
+    {:ok, _} = Ethereum.unlock_account(address, "pass")
+    {:ok, _} = Ethereum.fund_address_from_default_faucet(user, [])
+
     user
   end
 
   defp get_block!(blknum, child_chain_url) do
-    {block_hash, _} = RootChain.blocks(blknum)
+    {block_hash, _} = Ethereum.block_hash(blknum)
     {:ok, block} = poll_get_block(block_hash, child_chain_url)
     block
   end
 
   defp to_utxo_position_list(block, opts) do
-    block.transactions
+    block["transactions"]
     |> Stream.with_index()
     |> Stream.flat_map(fn {tx, index} ->
-      transaction_to_output_positions(tx, block.number, index, opts)
+      transaction_to_output_positions(tx, block["blknum"], index, opts)
     end)
   end
 
@@ -95,26 +95,31 @@ defmodule OMG.Performance.Generators do
     filtered_address = opts[:owned_by]
 
     tx
-    |> Transaction.Recovered.recover_from!()
-    |> Transaction.get_outputs()
+    |> Transaction.recover()
+    |> get_outputs()
     |> Enum.filter(&(is_nil(filtered_address) || &1.owner == filtered_address))
     |> Enum.with_index()
     |> Enum.map(fn {_, oindex} ->
-      utxo_pos = Utxo.position(blknum, txindex, oindex)
-      Utxo.Position.encode(utxo_pos)
+      ExPlasma.Utxo.pos(%{blknum: blknum, txindex: txindex, oindex: oindex})
     end)
+  end
+
+  defp get_outputs(transaction) do
+    transaction.raw_tx.outputs
   end
 
   defp poll_get_block(block_hash, child_chain_url) do
     poll_get_block(block_hash, child_chain_url, 50)
   end
 
-  defp poll_get_block(block_hash, child_chain_url, 0), do: Client.get_block(block_hash, child_chain_url)
-
   defp poll_get_block(block_hash, child_chain_url, retry) do
-    case Client.get_block(block_hash, child_chain_url) do
-      {:ok, _block} = result ->
-        result
+    client = LoadTest.Connection.ChildChain.client()
+    params = %ChildChainAPI.Model.GetBlockBodySchema{hash: Encoding.to_hex(block_hash)}
+    response = ChildChainAPI.Api.Block.block_get(client, params)
+
+    case response do
+      {:ok, block_response} ->
+        {:ok, Jason.decode!(block_response.body)["data"]}
 
       _ ->
         Process.sleep(10)
