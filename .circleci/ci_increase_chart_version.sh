a428fa1e94ff2e52c270f2ef0217de1a94cb013d
boolafish <boolafish945@gmail.com>
2020-09-18 15:53:23 +0900

    move dev env deployment job to helm repo (#1738)
    * feat: trigger increase chart version on merge to master
    
    * debug: switch to playground repo
    
    And allow circle ci to run on non-master
    
    * fix: unbound string
    
    * feat: add jitter to the call
    
    * remove jitter, not useful
    
    * chore: cleanup
    
    Uncomment CircleCI jobs, remove deployment jobs/scripts, not using playground repo
    
    * fix: circle ci format
    
    * fix: notifiy service do not wait for non-existing deployment job
    
    * chore: remove childchain master deployment
    
    Responsibility has been moved to omg-childhchain-v1 repo.
    Have another PR for that: https://github.com/omgnetwork/omg-childchain-v1/pull/15
    
    * chore: remove childchain release from elixir-omg
    
    Moved to omg-childchain-v1.

--- .circleci/ci_increase_chart_version.sh
+++ .circleci/ci_increase_chart_version.sh
@@ -1,29 +1,40 @@
 #!/bin/sh
 
 """
-This is the script that would send a dispatch event to the helm chart repo to
-auto increase chart version.
+This is the script that would send a dispatch event to the helm chart repo to auto increase chart version.
+You can set the UPDATE_DEV flag to decide whether to update to dev too.
+
+For master, we increase the chart version and update dev together. The app version should be short git sha with length 7.
+For release, we increase the chart version only. The app version should be semver. (eg. 1.0.3-pre.0)
 
 Required env vars:
-- CIRCLE_TAG
-- CHART_NAME
-- HELM_CHART_REPO
+- CHART_NAME (eg. childchain, watcher, watcher-info)
+- APP_VERSION (eg. 3d75118 or 1.0.3-pre.0)
+- HELM_CHART_REPO (eg. helm-devlopement)
+- UPDATE_DEV (true/false)
 - GITHUB_API_TOKEN
 """
 
-set -e
-set -x
+set -ex
 
-increase_chart_version() {
-    APP_VERSION="${CIRCLE_TAG#*v}"
+[ -z "$CHART_NAME" ] && echo "CHART_NAME should be set" && exit 1
+[ -z "$APP_VERSION" ] && echo "APP_VERSION should be set" && exit 1
+[ -z "$HELM_CHART_REPO" ] && echo "HELM_CHART_REPO should be set" && exit 1
+[ -z "$UPDATE_DEV" ] && echo "HELM_CHART_REPO should be set" && exit 1
+[ -z "$GITHUB_API_TOKEN" ] && echo "GITHUB_API_TOKEN should be set" && exit 1
 
-    echo "increase chart version for chart: ${CHART_NAME} with APP_VERSION: ${APP_VERSION}"
 
-    curl --location --request POST 'https://api.github.com/repos/omgnetwork/'${HELM_CHART_REPO}'/dispatches' \
-    --header 'Accept: application/vnd.github.v3+json' \
-    --header 'authorization: token '${GITHUB_API_TOKEN}'' \
-    --header 'Content-Type: application/json' \
-    --data-raw '{"event_type": "increase-chart-version", "client_payload": { "chart_name": "'${CHART_NAME}'", "app_version": "'${APP_VERSION}'" }}'
-}
+echo "increase chart version: chart [${CHART_NAME}], appVersion: [${APP_VERSION}], update_dev: [${UPDATE_DEV}]"
 
-increase_chart_version
+curl --location --request POST "https://api.github.com/repos/omgnetwork/${HELM_CHART_REPO}/dispatches" \
+--header "Accept: application/vnd.github.v3+json" \
+--header "authorization: token ${GITHUB_API_TOKEN}" \
+--header "Content-Type: application/json" \
+--data-raw " { \
+    \"event_type\": \"increase-chart-version\", \
+    \"client_payload\": { \
+        \"chart_name\": \"${CHART_NAME}\", \
+        \"app_version\": \"${APP_VERSION}\", \
+        \"update_dev\": \"${UPDATE_DEV}\" \
+    } \
+}"
