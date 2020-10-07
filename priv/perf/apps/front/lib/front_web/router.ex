8f0f9fe3b67fe04fcc06d29a249d26e7b1f8c266
Ayrat Badykov <ayratin555@gmail.com>
2020-10-07 17:23:39 +0300

    format code

--- priv/perf/apps/front/lib/front_web/router.ex
+++ priv/perf/apps/front/lib/front_web/router.ex
@@ -2,11 +2,11 @@ defmodule FrontWeb.Router do
   use FrontWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", FrontWeb do
-    pipe_through :api
+    pipe_through(:api)
   end
 
   # Enables LiveDashboard only for development
@@ -20,8 +20,8 @@ defmodule FrontWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: FrontWeb.Telemetry
+      pipe_through([:fetch_session, :protect_from_forgery])
+      live_dashboard("/dashboard", metrics: FrontWeb.Telemetry)
     end
   end
 end
