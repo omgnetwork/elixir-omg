ba618f1972cc8e37a878a20350bb185da5fe6d6b
Pawel Gebal <pawel.gebal@imapp.pl>
2020-10-23 14:41:00 +0200

    fix: fixes before CR

--- apps/omg_conformance/test/support/conformance/signatures_hashes.ex
+++ apps/omg_conformance/test/support/conformance/signatures_hashes.ex
@@ -19,6 +19,7 @@ defmodule Support.Conformance.SignaturesHashes do
 
   import ExUnit.Assertions, only: [assert: 1]
 
+  alias OMG.Eth.Configuration
   alias OMG.Eth.Encoding
   alias OMG.State.Transaction
 
@@ -133,10 +134,12 @@ defmodule Support.Conformance.SignaturesHashes do
   defp call_contract(contract, signature, args, return_types) do
     data = ABI.encode(signature, args)
 
-    from = Configuration.authority_address()
-
     {:ok, return} =
-      Ethereumex.HttpClient.eth_call(%{from: from, to: Encoding.to_hex(contract), data: Encoding.to_hex(data)})
+      Ethereumex.HttpClient.eth_call(%{
+        from: Configuration.authority_address(),
+        to: Encoding.to_hex(contract),
+        data: Encoding.to_hex(data)
+      })
 
     decode_answer(return, return_types)
   end
--- priv/perf/config/config.exs
+++ priv/perf/config/config.exs
@@ -30,7 +30,8 @@ config :load_test,
   faucet_deposit_amount: trunc(:math.pow(10, 14)),
   fee_amount: 1,
   deposit_finality_margin: 10,
-  gas_price: 2_000_000_000
+  gas_price: 2_000_000_000,
+  eth_call_from_address: "0x0000000000000000000000000000000000000001"
 
 config :ex_plasma,
   eip_712_domain: [
