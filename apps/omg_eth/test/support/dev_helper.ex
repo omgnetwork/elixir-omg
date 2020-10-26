321f182439c26b90e611466e5ad17ba83ab57501
Pawel Gebal <pawel.gebal@imapp.pl>
2020-10-26 14:02:52 +0100

    Revert "fix: fix before CR"
    This reverts commit 26467f0b270a4b650fe75a0c5606d3c4a4bb1341.

--- apps/omg_eth/test/support/dev_helper.ex
+++ apps/omg_eth/test/support/dev_helper.ex
@@ -157,6 +157,7 @@ defmodule Support.DevHelper do
     {:ok, call_result} =
       tx
       |> Map.put("data", tx["input"])
+      |> Map.put("from", Configuration.authority_address())
       |> Ethereumex.HttpClient.eth_call(tx["blockNumber"])
 
     # this call result is hex decoded and then additionally decoded with ABI, should yield a readable ascii-string
