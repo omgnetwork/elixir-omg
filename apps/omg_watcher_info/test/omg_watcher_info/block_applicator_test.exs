842119f8327996d7fc3d16b08b198b7d42b24583
Pawel Nowosielski <Pawel.Nowosielski@imapp.pl>
2020-11-20 14:50:16 +0100

    chore: delete unused modules, linter fixes

--- apps/omg_watcher_info/test/omg_watcher_info/block_applicator_test.exs
+++ apps/omg_watcher_info/test/omg_watcher_info/block_applicator_test.exs
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 OmiseGO Pte Ltd
+# Copyright 2019-2020 OMG Network Pte Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,16 +21,16 @@ defmodule OMG.WatcherInfo.BlockApplicatorTest do
   alias OMG.WatcherInfo.DB
 
   setup do
-      block_application = %BlockApplication{
-        number: 1_000,
-        eth_height: 1,
-        eth_height_done: true,
-        hash: "0x1000",
-        transactions: [],
-        timestamp: 1_576_500_000
-      }
+    block_application = %BlockApplication{
+      number: 1_000,
+      eth_height: 1,
+      eth_height_done: true,
+      hash: "0x1000",
+      transactions: [],
+      timestamp: 1_576_500_000
+    }
 
-      {:ok, block_application: block_application}
+    {:ok, block_application: block_application}
   end
 
   describe "insert_block!" do
